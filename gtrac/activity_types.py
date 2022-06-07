8215a30b9538dbf8087fe5288fac51d306d1ce77
fortysix2ahead <fortysix2ahead@gmail.com>
2022-06-07 14:02:36 +0200

    Added types + utils
    Signed-off-by: fortysix2ahead <fortysix2ahead@gmail.com>

---
+++ gtrac/activity_types.py
@@ -0,0 +1,145 @@
+
+from enum import Enum
+
+class ActivityTypes( Enum ):
+
+	aerobics = 'aerobics'
+	badminton = 'badminton'
+	ballet = 'ballet'
+	baseball = 'baseball'
+	basketball = 'basketball'
+	biathlon = 'biathlon'
+	bike = 'bike'
+	bike_ebike = 'bike_ebike'
+	bike_ergo = 'bike_ergo'
+	bike_hand = 'bike_hand'
+	bike_mountain = 'bike_mountain'
+	bike_road = 'bike_road'
+	canoe = 'canoe'
+	climb = 'climb'
+	crossfit = 'crossfit'
+	drive = 'drive'
+	ergo = 'ergo'
+	golf = 'golf'
+	gym = 'gym'
+	gymnastics = 'gymnastics'
+	hike = 'hiking'
+	ice_skate = 'ice_skate'
+	inline_skate = 'inline_skate'
+	kayak = 'kayak'
+	kitesurf = 'kitesurf'
+	multisport = 'multisport'
+	paddle = 'paddle'
+	paddle_standup = 'paddle_standup'
+	rollski = 'rollski'
+	rollski_classic = 'rollski_classic'
+	rollski_free = 'rollski_free'
+	row = 'row'
+	row_ergo = 'row_ergo'
+	run = 'run'
+	run_baby = 'run_baby'
+	run_ergo = 'run_ergo'
+	sail = 'sail'
+	soccer = 'soccer'
+	skateboard = 'soccer'
+	ski = 'ski'
+	snowboard = 'snowboard'
+	snowshoe = 'snowshoe'
+	swim = 'swim'
+	swim_indoor = 'swim_indoor'
+	swim_outdoor = 'swim_outdoor'
+	surf = 'surf'
+	surf_wind = 'surf_wind'
+	test = 'test'
+	triathlon = 'triathlon'
+	walk = 'walk'
+	xcski = 'xcski'
+	xcski_backcountry = 'xcski_backcountry'
+	xcski_classic = 'xcski_classic'
+	xcski_free = 'xcski_free'
+	yoga = 'yoga'
+	other = 'other'
+	unknown = 'unknown'
+
+	@classmethod
+	def get( cls, value: str ) -> Enum:
+		for item in cls.items():
+			if item[1] == value:
+				return cls[item[0]]
+		return cls['unknown']
+
+	@classmethod
+	def items( cls ):
+		return list( map( lambda c: (c.name, c.value), cls ) )
+
+	@classmethod
+	def names( cls ):
+		return list( map( lambda c: c.name, cls ) )
+
+	@classmethod
+	def values( cls ):
+		return list( map( lambda c: c.value, cls ) )
+
+	@property
+	def display_name( self ):
+		return display_names.get( self, ActivityTypes.unknown )
+
+display_names = {
+	ActivityTypes.aerobics: 'Aerobics',
+	ActivityTypes.badminton: 'Badminton',
+	ActivityTypes.ballet: 'Ballet',
+	ActivityTypes.baseball: 'Baseball',
+	ActivityTypes.basketball: 'Basketball',
+	ActivityTypes.biathlon: 'Biathlon',
+	ActivityTypes.bike: 'Cycling',
+	ActivityTypes.bike_ebike: 'E-Biking',
+	ActivityTypes.bike_ergo: 'Ergometer',
+	ActivityTypes.bike_hand: 'Handbiking',
+	ActivityTypes.bike_mountain: 'Mountain Biking',
+	ActivityTypes.bike_road: 'Road Cycling',
+	ActivityTypes.canoe: 'Canoe',
+	ActivityTypes.climb: 'Climbing',
+	ActivityTypes.crossfit: 'Crossfit',
+	ActivityTypes.drive: 'Driving',
+	ActivityTypes.ergo: 'Ergotrainer',
+	ActivityTypes.golf: 'Golf',
+	ActivityTypes.gym: 'Strength Training',
+	ActivityTypes.gymnastics: 'Gymnastics',
+	ActivityTypes.hike: 'Hiking',
+	ActivityTypes.ice_skate: 'Ice Skating',
+	ActivityTypes.inline_skate: 'Inline Skating',
+	ActivityTypes.kayak: 'Kayak',
+	ActivityTypes.kitesurf: 'Kitesurf',
+	ActivityTypes.multisport: 'Multisport',
+	ActivityTypes.paddle: 'Paddling',
+	ActivityTypes.paddle_standup: 'Standup Paddling',
+	ActivityTypes.rollski: 'Roller Skiing',
+	ActivityTypes.rollski_classic: 'Roller Skiing - Classic',
+	ActivityTypes.rollski_free: 'Roller Skiing - Freestyle',
+	ActivityTypes.row: 'Rowing',
+	ActivityTypes.row_ergo: 'Rowing Ergometer',
+	ActivityTypes.run: 'Run',
+	ActivityTypes.run_baby: 'Run with Babyjogger',
+	ActivityTypes.run_ergo: 'Treadmill Run',
+	ActivityTypes.sail: 'Sailing',
+	ActivityTypes.soccer: 'Soccer',
+	ActivityTypes.skateboard: 'Skateboard',
+	ActivityTypes.ski: 'Alpine Ski',
+	ActivityTypes.snowboard: 'Snowboard',
+	ActivityTypes.snowshoe: 'Snowshoe',
+	ActivityTypes.swim: 'Swimming',
+	ActivityTypes.swim_indoor: 'Indoor Swimming',
+	ActivityTypes.swim_outdoor: 'Openwater Swimming',
+	ActivityTypes.surf: 'Surfing',
+	ActivityTypes.surf_wind: 'Windsurfing',
+	ActivityTypes.test: 'Fitness Test',
+	ActivityTypes.triathlon: 'Triathlon',
+	ActivityTypes.walk: 'Walking',
+	ActivityTypes.xcski: 'Cross Country Skiing',
+	ActivityTypes.xcski_backcountry: 'Cross Country Skiing - Backcountry',
+	ActivityTypes.xcski_classic: 'Cross Country Skiing - Classic',
+	ActivityTypes.xcski_free: 'Cross Country Skiing - Freestyle',
+	ActivityTypes.yoga: 'Yoga',
+	ActivityTypes.other: 'Other',
+	ActivityTypes.unknown: 'Unknown',
+}
---
+++ gtrac/utils.py
@@ -0,0 +1,171 @@
+
+from datetime import date
+from datetime import datetime
+from datetime import time
+from datetime import timedelta
+from datetime import timezone
+from datetime import tzinfo
+from enum import Enum
+from re import match
+from time import gmtime
+from typing import Callable
+from typing import Dict
+from typing import List
+from typing import Optional
+from typing import Tuple
+from typing import Union
+
+from babel.dates import format_datetime
+from babel.dates import format_date
+from babel.dates import format_time
+from babel.dates import format_timedelta
+from babel.numbers import format_decimal
+from babel.dates import get_timezone
+from click import style
+from dateutil.parser import parse as parse_datetime
+from dateutil.parser import ParserError
+from dateutil.tz import gettz
+
+from .activity_types import ActivityTypes
+from .config import ApplicationConfig as cfg
+
+# custom types
+
+FunctionDict = Dict[Union[str, Tuple[str, str]], Callable]
+
+# default formats
+
+_LOCALE = 'en'
+_DATE_FORMAT = 'medium'
+_DATETIME_FORMAT = 'medium'
+_TIME_FORMAT = 'medium'
+_TIMEDELTA_FORMAT = 'short'
+
+_DTISO = r'^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(\.[0-9]+)?(Z|[+-](?:2[0-3]|[01][0-9]):[0-5][0-9])?$'
+
+def fmt( value, locale = None ) -> str:
+	_locale = locale or cfg['formats']['locale'].get( _LOCALE )
+	_date_fmt = cfg['formats']['date'].get() or _DATE_FORMAT
+	_datetime_fmt = cfg['formats']['datetime'].get() or _DATETIME_FORMAT
+	_time_fmt = cfg['formats']['time'].get() or _TIME_FORMAT
+	_timedelta_fmt = cfg['formats']['timedelta'].get() or _TIMEDELTA_FORMAT
+	_rval = ''
+
+	if value is None or value == '':
+		_r_val = ''
+
+	if isinstance( value, str ):
+		if match( '^\d+$', value ): # format integer
+			value = int( value )
+		elif match( '^\d+\.\d+$', value ): # format float
+			value = float( value )
+		elif match( _DTISO, value ): # iso datetime
+			value = datetime.fromisoformat( value )
+		else:
+			_rval = value
+
+	if type( value ) is int:
+		_rval = str( value )
+
+	if type( value ) is float:
+		_rval = format_decimal( value, format='#,###.#', locale=_locale )
+
+	if type( value ) is datetime:
+		_rval = format_datetime( value, locale=_locale, format=_time_fmt )
+
+	if type( value ) is date:
+		_rval = format_date( value, locale=_locale, format=_time_fmt )
+
+	if type( value ) is time:
+		_rval = format_time( value, locale=_locale, format=_time_fmt )
+
+	if type( value ) is timedelta:
+		_rval = format_timedelta( value, locale=_locale, format=_timedelta_fmt, granularity='second', threshold=3 )
+		#_rval = format_timedelta( value, locale=_locale, format=_timedelta_fmt, granularity='second', add_direction=True )
+
+	if type( value ) is ActivityTypes:
+		_rval = value.display_name
+	elif isinstance( value, Enum ):
+		_rval = value.value
+
+	return _rval
+
+def fmtl( activity_list: List ) -> str:
+	"""
+	Returns a list of ids taken from the provided list of activities.
+
+	:param activity_list: list of activities
+	:return: string with a list of ids of the activities
+	"""
+	return f"[{', '.join( [str( a.id ) for a in activity_list or []] )}]"
+
+def fmt_delta( dt1: datetime, dt2: datetime ) -> str:
+	return f'{fmt( dt1 )} (\u00B1{fmt( dt1 - dt2 )})'
+
+def as_datetime( dt: datetime = None, dtstr: str = None, ts: int = 0, tz: tzinfo = None, tzstr: str = None ) -> datetime:
+	tz = gettz( tzstr ) if tzstr else tz # construct tz from tzstr
+	tz = tz if tz else timezone.utc # tz wins over tzstr
+
+	ts = ts / 1000 if ts > 4102444800 else ts # treat ts > year 2100 in milliseconds
+	_dt = datetime.fromtimestamp( ts, tz ) if ts > 0 else None
+	_dt = parse_datetime( dtstr ) if dtstr else _dt # iso string wins over ts
+	_dt = dt if dt else _dt # dt wins over iso string
+	_dt = _dt.astimezone( tz ) if _dt else None # return None if everything fails
+	return _dt
+
+def as_time( tstr: str = None ) -> time:
+	_t = time.fromisoformat( tstr ) if tstr else None
+	return _t
+
+def delta( a: time, b: time ) -> timedelta:
+	return datetime.combine( date.min, a ) - datetime.combine( date.min, b )
+
+def seconds_to_time( time_float: float ) -> Optional[time]:
+	if not isinstance( time_float, (float, int) ):
+		return None
+	gt = gmtime( round( time_float, 0 ) )
+	return time( gt.tm_hour, gt.tm_min, gt.tm_sec )
+
+def to_isotime( timestr: str ) -> Optional[datetime]:
+	try:
+		return parse_datetime( timestr )
+	except (ParserError, TypeError):
+		return None
+
+def fromtimezone( value ) -> time:
+	return get_timezone( value ) if value else get_timezone()
+
+def fromisoformat( value ) -> Optional[datetime] or Optional[time]:
+	rval = None
+	if type( value ) in [time, datetime]:
+		rval = value
+	elif type( value ) is str:
+		try:
+			rval = time.fromisoformat( value )
+		except ValueError:
+			try:
+				rval = parse_datetime( value )
+			except ParserError or OverflowError:
+				pass
+	return rval
+
+def toisoformat( value ) -> Optional[str]:
+	if type( value ) in [time, datetime]:
+		return value.isoformat()
+	return value # todo: or return None?
+
+def serialize( value ) -> Optional[str]:
+	if type( value ) in [time, datetime]:
+		return toisoformat( value )
+	elif isinstance( value, Enum ):
+		return value.name
+	else:
+		return value
+
+# styling helpers
+
+def blue( s: str ) -> str:
+	return style( s, fg='blue' )
+
+def red( s: str ) -> str:
+	return style( s, fg='red' )

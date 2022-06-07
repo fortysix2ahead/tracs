b68bd5b207555ac0b9b761b8540c2eff697548c7
fortysix2ahead <fortysix2ahead@gmail.com>
2022-06-07 14:27:14 +0200

    Added base + activity module
    Signed-off-by: fortysix2ahead <fortysix2ahead@gmail.com>

---
+++ gtrac/activity.py
@@ -0,0 +1,118 @@
+
+from __future__ import annotations
+
+from datetime import datetime
+from typing import Dict
+from typing import Union
+
+from attrs import define
+from attrs import field
+from logging import getLogger
+from typing import List
+from typing import Optional
+
+from .activity_types import ActivityTypes
+from .config import KEY_GROUPS
+from .dataclasses import BaseDocument
+from .dataclasses import PERSIST
+from .dataclasses import PERSIST_AS
+from .dataclasses import PROTECTED
+from .dataclasses import str2datetime
+from .dataclasses import str2time
+
+log = getLogger( __name__ )
+
+# noinspection PyTypeChecker
+def str2activitytype( v: Union[ActivityTypes, str] ) -> ActivityTypes:
+	if type( v ) is ActivityTypes:
+		return v
+	elif type( v ) is str:
+		return ActivityTypes.get( v )
+	else:
+		return None
+
+@define
+class Resource:
+
+	name: str = field( init=True, default=None )
+
+@define
+class ActivityRef:
+
+	id: str = field( init=True, default=None )
+	uid: str = field( init=True, default=None )
+
+	classifier: str = field( init=False, default=None )
+	raw_id: str = field( init=False, default=None )
+
+	def __attrs_post_init__( self ):
+		self.classifier, self.raw_id = self.uid.split( ':' ) if self.uid else (None, None)
+
+@define( init=True )
+class Activity( BaseDocument ):
+
+	name: str = field( init=True, default=None ) # activity name
+	type: ActivityTypes = field( init=True, default=None, converter=str2activitytype ) # activity type
+	description: str = field( init=True, default=None ) # description
+	tags: List[str] = field( init=True, default=[] ) # list of tags
+	location_country: str = field( init=True, default=None ) #
+	location_state: str = field( init=True, default=None ) #
+	location_city: str = field( init=True, default=None ) #
+	location_place: str = field( init=True, default=None ) #
+	route: str = field( init=True, default=None ) #
+
+	time: datetime = field( init=True, default=None, converter=str2datetime ) # activity time (UTC)
+	localtime: datetime = field( init=True, default=None, converter=str2datetime ) # activity time (local)
+	timezone: str = field( init=True, default=None ) #
+	duration: time = field( init=True, default=None, converter=str2time ) #
+	duration_moving: time = field( init=True, default=None, converter=str2time ) #
+
+	distance: float = field( init=True, default=None ) #
+	ascent: float = field( init=True, default=None ) #
+	descent: float = field( init=True, default=None ) #
+	elevation_max: float = field( init=True, default=None ) #
+	elevation_min: float = field( init=True, default=None ) #
+	speed: float = field( init=True, default=None ) #
+	speed_max: float = field( init=True, default=None ) #
+
+	heartrate: float = field( init=True, default=None ) #
+	heartrate_max: float = field( init=True, default=None ) #
+	heartrate_min: float = field( init=True, default=None ) #
+	calories: float = field( init=True, default=None ) #
+
+	groups: Dict = field( init=True, default={}, metadata={ PERSIST_AS: KEY_GROUPS, PROTECTED: True } ) # todo: for backward compatibility
+	metadata: Dict = field( init=True, default={}, metadata={ PERSIST_AS: '_metadata', PROTECTED: True } )
+	resources: List[Resource] = field( init=True, default=[], metadata={ PERSIST_AS: '_resources', PROTECTED: True } )
+
+	parent: Activity = field( init=True, default=None, metadata={ PERSIST: False, PROTECTED: True } )
+	parent_ref: ActivityRef = field( init=True, default=None, metadata={ PERSIST: False, PROTECTED: True } )
+	parent_id: int = field( init=True, default=None, metadata={ PERSIST_AS: '_parent_id', PROTECTED: True } )
+	parent_uid: str = field( init=True, default=None, metadata={ PERSIST_AS: '_parent_uid', PROTECTED: True } )
+
+	is_group: bool = field( init=False, default=False, metadata={ 'persist': False } ) # todo: for backward compatibility
+	is_multipart: bool = field( init=False, default=False, metadata={ 'persist': False } ) # todo: for backward compatibility
+
+	def __attrs_post_init__( self ):
+		super().__attrs_post_init__()
+
+		# todo: for backward compatibility
+		if isinstance( self.groups, dict ) and 'parent' in self.groups:
+			self.parent_id = self.groups['parent']
+			self.parent_uid = f"group:{self.groups['parent']}"
+			self.parent_ref = ActivityRef( self.parent_id, self.parent_uid )
+
+class MultipartActivity( Activity ):
+
+	parts: List[Activity] = field( init=True, default=[], metadata={ 'persist_as': '_parts' } )
+
+#	@property
+#	def parts( self ) -> Mapping:
+#		return self._access_map( KEY_PARTS )
+
+	@property
+	def part_for( self ) -> List[int]:
+		return self.parts.get( 'ids', [] )
+
+	@property
+	def part_of( self ) -> Optional[int]:
+		return self.parts.get( 'parent', None )
---
+++ gtrac/base.py
@@ -0,0 +1,122 @@
+
+from __future__ import annotations
+
+from abc import ABC
+from abc import abstractmethod
+from dataclasses import dataclass
+from dataclasses import field
+from pathlib import Path
+from typing import Any
+from typing import Dict
+from typing import List
+from typing import Optional
+
+@dataclass
+class Resource:
+
+	parent: Dict = field( default=None )
+	name: str = field( default=None )
+	type: str = field( default=None )
+	status: int = field( default=100 )
+	path: str = field( default=None )
+	#path: Path = field( default=None )
+
+	def __setattr__( self, name: str, value: Any ) -> None:
+		super().__setattr__( name, value )
+		if name != 'parent' and self.parent is not None:
+			self.parent[name] = value
+
+class Activity( ABC, dict ):
+	"""
+	Abstract base class for an activity.
+	"""
+
+	@property
+	@abstractmethod
+	def id( self ) -> int:
+		pass
+
+	@property
+	@abstractmethod
+	def uid( self ) -> str:
+		pass
+
+	@property
+	@abstractmethod
+	def raw_id( self ) -> int:
+		pass
+
+	@property
+	@abstractmethod
+	def resources( self ) -> List[Resource]:
+		pass
+
+class Service( ABC ):
+	"""
+	Abstract base class for a service being able to consume activities from external sources.
+	"""
+
+	@abstractmethod
+	def login( self ) -> bool:
+		pass
+
+	@abstractmethod
+	def path_for( self, a: Activity, ext: Optional[str] = None ) -> Optional[Path]:
+		"""
+		Returns the path in the local file system where all artefacts of a provided activity are located.
+		The returned path must point to a directory.
+
+		:param a: activity
+		:param ext: file extension for which the path should be returned, can be None
+		:return: path of the activity in the local file system
+		"""
+		pass
+
+	@abstractmethod
+	def link_for( self, a: Activity, ext: Optional[str] = None ) -> Optional[Path]:
+		"""
+		Returns the link path for an activity.
+
+		:param a: activity to link
+		:param ext: file extension
+		:return: link path for activity
+		"""
+		pass
+
+	@abstractmethod
+	def fetch( self, fetch_all: bool ) -> [Activity]:
+		pass
+
+	@abstractmethod
+	def download( self, activity: Activity, force: bool, pretend: bool ) -> None:
+		pass
+
+	@abstractmethod
+	def link( self, activity: Activity, force: bool, pretend: bool ) -> None:
+		pass
+
+	@abstractmethod
+	def setup( self ) -> None:
+		pass
+
+	# define abstract properties
+
+	@property
+	@abstractmethod
+	def logged_in( self ) -> bool:
+		pass
+
+	@property
+	@abstractmethod
+	def enabled( self ) -> bool:
+		pass
+
+	@property
+	@abstractmethod
+	def name( self ) -> str:
+		pass
+
+	@property
+	@abstractmethod
+	def display_name( self ) -> str:
+		pass

d13fa6fe27948e832df98623271a063e4b09197c
fortysix2ahead <fortysix2ahead@gmail.com>
2022-06-08 14:09:31 +0200

    Added test case for polar server
    Signed-off-by: fortysix2ahead <fortysix2ahead@gmail.com>

---
+++ test/polar_server.py
@@ -0,0 +1,41 @@
+
+from threading import Thread
+
+from bottle import Bottle
+from bottle import static_file
+
+from pytest import fixture
+
+from .helpers import get_file_path
+
+TEST_HOST = 'localhost'
+TEST_PORT = 40080
+
+server = Bottle()
+server_function = server.run
+server_args = {
+    'host': TEST_HOST,
+    'port': TEST_PORT,
+    'debug': True,
+}
+server_thread = Thread(target=server_function, kwargs=server_args, daemon=True)
+
+@server.get('/')
+def root():
+    return '<p>Hello</p>'
+
+@server.get('/ajaxLogin')
+def ajax_login():
+    return static_file( 'ajaxlogin.html', root=get_file_path( 'templates/polar' ), mimetype='text/html' )
+
+@server.post('/login')
+def login():
+    return '<p>Success</p>'
+
+# fixture starting a background server
+
+@fixture
+def polar_server() -> Bottle:
+    if not server_thread.is_alive():
+        server_thread.start()
+    return server
---
+++ test/test_polar_service.py
@@ -0,0 +1,39 @@
+
+from tracs.config import ApplicationConfig as cfg
+from tracs.config import KEY_PLUGINS
+from tracs.plugins.polar import Polar
+
+from .polar_server import polar_server
+
+LIVE_BASE_URL = 'https://flow.polar.com'
+TEST_BASE_URL = 'http://localhost:40080'
+
+def test_constructor():
+	polar = Polar()
+
+	assert polar.base_url == f'{LIVE_BASE_URL}'
+	assert polar.login_url == f'{LIVE_BASE_URL}/login'
+	assert polar.login_ajax_url.startswith( f'{LIVE_BASE_URL}/ajaxLogin?_=' )
+	assert polar.events_url == f'{LIVE_BASE_URL}/training/getCalendarEvents'
+	assert polar.export_url == f'{LIVE_BASE_URL}/api/export/training'
+
+	polar.base_url = TEST_BASE_URL
+
+	assert polar.base_url == f'{TEST_BASE_URL}'
+	assert polar.login_url == f'{TEST_BASE_URL}/login'
+	assert polar.login_ajax_url.startswith( f'{TEST_BASE_URL}/ajaxLogin?_=' )
+	assert polar.events_url == f'{TEST_BASE_URL}/training/getCalendarEvents'
+	assert polar.export_url == f'{TEST_BASE_URL}/api/export/training'
+
+def test_login( polar_server ):
+	# create service
+	polar = Polar()
+	polar.base_url = TEST_BASE_URL
+
+	# configure credentials
+	cfg[KEY_PLUGINS]['polar']['username'] = 'sample user'
+	cfg[KEY_PLUGINS]['polar']['password'] = 'sample password'
+
+	polar.login()
+
+	assert polar.logged_in

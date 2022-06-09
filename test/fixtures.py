805410ebec8ed602914e88fbab91bedccd8b5ce2
fortysix2ahead <fortysix2ahead@gmail.com>
2022-06-09 15:02:35 +0200

    Added initial strava server test case
    Signed-off-by: fortysix2ahead <fortysix2ahead@gmail.com>

--- test/fixtures.py
+++ test/fixtures.py
@@ -1,5 +1,6 @@
 from importlib.resources import path
 from pathlib import Path
+from typing import Dict
 from typing import Mapping
 from typing import Optional
 from typing import Tuple
@@ -9,6 +10,7 @@ from yaml import SafeLoader
 from yaml import load as load_yaml
 
 from tracs.config import ApplicationConfig as cfg
+from tracs.config import ApplicationState as state
 from tracs.config import GlobalConfig
 from tracs.db import ActivityDb
 from tracs.plugins import Registry
@@ -56,6 +58,26 @@ def var_config( request ) -> bool:
 				return True
 
 	return False
+
+@fixture
+def config_state( request ) -> Optional[Tuple[Dict, Dict]]:
+	config_dict, state_dict = None, None
+	config_marker = request.node.get_closest_marker( 'config_file' )
+	state_marker = request.node.get_closest_marker( 'state_file' )
+	if config_marker and state_marker:
+		with path( 'test', '__init__.py' ) as test_pkg_path:
+			config_path = Path( test_pkg_path.parent.parent, 'var', config_marker.args[0] )
+			config = None
+			if config_path.exists():
+				cfg.set_file( config_path )
+				config_dict = load_yaml( config_path.read_bytes(), SafeLoader )
+
+			state_path = Path( test_pkg_path.parent.parent, 'var', state_marker.args[0] )
+			if state_path.exists():
+				state.set_file( state_path )
+				state_dict = load_yaml( state_path.read_bytes(), SafeLoader )
+
+	return config_dict, state_dict
 
 @fixture
 def var_dir() -> Path:

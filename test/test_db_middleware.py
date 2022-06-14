2fa37b1e9d51a9eb82944c6042e9ec604c8fa95f
fortysix2ahead <fortysix2ahead@gmail.com>
2022-06-14 14:05:58 +0200

    Refactoring on test cases

--- test/test_db_middleware.py
+++ test/test_db_middleware.py
@@ -1,123 +1,61 @@
 
-from datetime import datetime
+from typing import cast
 
-from dateutil.tz import UTC
-from dateutil.tz import tzlocal
-
+from pytest import mark
 from tinydb import TinyDB
-from tinydb.operations import set
 from tinydb.storages import MemoryStorage
 
-from tracs.activity_types import ActivityTypes
-from tracs.db import DB_VERSION
-from tracs.db_storage import OrJSONStorage
+from tracs.db import document_cls
+from tracs.db_middleware import DataClassMiddleware
+from tracs.plugins.polar import PolarActivity
 
-from .helpers import get_db_path
-
-table_data = {
-	'_default': {
-		'1': {
-			'version': 10
-		}
-	},
-	'activities': {
-		'1': {
-			'_classifier': 'polar',
-			'_raw': {
-				'type': 'Hiking',
-			},
-			'name': 'name',
-			'type': 'hike',
-			'__test_field': 'field name should never be created, just for testing',
-		},
-		'2': {
-			'_classifier': 'polar',
-			'_raw': {
-				'type': 'Hiking',
-			},
-		},
-		'3': {
-			'_classifier': 'polar',
-			'_raw': {},
-			'type': 'hike',
-		}
-	}
-}
-
-def test_read_data():
-
-	storage_cls = MemoryStorage
-	storage_cls = ClassMapMiddleware( storage_cls, { 'activities': ClassifiedDocument } )
+@mark.db_template( 'default' )
+def test_read_data( json ):
+	storage_cls = DataClassMiddleware( MemoryStorage )
 	db = TinyDB( storage=storage_cls )
 
 	# db setup, populate memory
-	ccm = db.storage
-	ms = ccm.storage
-	ms.memory = table_data
+	mw = cast( DataClassMiddleware, db.storage )
+	ms = cast( MemoryStorage, mw.storage )
+	ms.memory = json
 
-	d = ccm.read().get( 'activities' ).get( '1' )
-	assert type( d ) == ClassifiedDocument
+	# setup transmap
+	mw.transmap['activities'] = document_cls
 
-	d = ccm.read().get( '_default' ).get( '1' )
-	assert type( d ) == dict
+	# try read operations
+	data = mw.read()
 
-def test_json_memory_storage():
-	db_path = get_db_path( 'default' )
-	db = TinyDB( path=db_path, storage=JSONMemoryStorage )
+	assert 'activities' in data
+	assert '2' in data['activities']
+	assert type( data['activities']['2'] ) is PolarActivity
 
-	assert db.table( '_default' ) is not None
-	assert db.table( 'activities' ) is not None
+def test_read_data_from_disk():
+	pass
 
-	d = db.table( '_default' ).get( doc_id=1 )
-	assert d['version'] == DB_VERSION
+@mark.db_template( 'default' )
+def test_write_data( db, json ):
+	polar_raw = json['activities']['2']['_raw']
+	polar = PolarActivity( raw=polar_raw )
 
-	db.table( '_default' ).update( set( 'version', 100 ), doc_ids=[1] )
-	d = db.table( '_default' ).get( doc_id=1 )
-	assert d['version'] == 100
+	# set up db
+	db = TinyDB( storage=DataClassMiddleware( MemoryStorage ) )
+	mw = cast( DataClassMiddleware, db.storage )
+	mw.transmap['activities'] = document_cls
+	ms = cast( MemoryStorage, mw.storage )
 
-def test_orjson_storage():
-	db_path = get_db_path( 'default' )
+	db.table( 'activities' ).insert( polar )
 
-	db = TinyDB( path=db_path, storage=OrJSONStorage )
-	assert db.table( '_default' ) is not None
-	assert db.table( 'activities' ) is not None
-	assert len( db.table( '_default' ).all() ) > 0
-	assert len( db.table( 'activities' ).all() ) > 0
-
-	db = TinyDB( path=db_path, storage=OrJSONStorage, use_memory_store=True )
-	assert db.table( '_default' ) is not None
-	assert db.table( 'activities' ) is not None
-	assert len( db.table( '_default' ).all() ) > 0
-	assert len( db.table( 'activities' ).all() ) > 0
-
-	db = TinyDB( path=None, storage=OrJSONStorage, use_memory_store=True )
-	assert db.table( '_default' ) is not None
-	assert db.table( 'activities' ) is not None
-	assert len( db.table( '_default' ).all() ) == 0
-	assert len( db.table( 'activities' ).all() ) == 0
-
-	cls_map = { 'activities': ClassifiedDocument }
-	db = TinyDB( path=db_path, storage=OrJSONStorage, use_memory_store=True, cls_map=cls_map )
-	db.table( 'activities' ).document_class = ClassifiedDocument
-
-	assert type( db.storage ) == OrJSONStorage
-	# noinspection PyUnresolvedReferences
-	d = db.storage.memory_storage.memory.get( 'activities' ).get( '1' )
-	assert type( d ) == ClassifiedDocument
-
-def test_orjson_write():
-	db_path = get_db_path( 'empty', True )
-	cls_map = { 'activities': ClassifiedDocument }
-	db = TinyDB( path=db_path, storage=OrJSONStorage, cls_map=cls_map )
-
-	doc = {
-		'date': datetime.utcnow().date(),
-		'datetime': datetime.utcnow(),
-		'utctime': datetime( 2020, 1, 2, 3, 4, 5, tzinfo=UTC ),
-		'localtime': datetime( 2020, 1, 2, 3, 4, 5, tzinfo=tzlocal() ),
-		'time': datetime.utcnow().time(),
-		'type': ActivityTypes.run,
+	assert ms.memory['activities']['1'] == {
+		'calories': 2904,
+		'_classifier': 'polar',
+		'distance': 30000.1,
+		'duration': '03:23:53',
+		'localtime': '2012-01-07T10:40:51+01:00',
+		'_metadata': {},
+		'name': '03:23:53;0.0 km',
+		'raw_id': 1234567890,
+		'_resources': [],
+		'tags': [],
+		'time': '2012-01-07T09:40:51+00:00',
+		'type': 'xcski_classic'
 	}
-
-	db.insert( doc )
-	db.close()

bfedeabc9f83b873a75e3fd00aab24dada6ac68d
fortysix2ahead <fortysix2ahead@gmail.com>
2022-06-14 15:25:46 +0200

    Updated middleware test

--- test/test_db_middleware.py
+++ test/test_db_middleware.py
@@ -1,10 +1,16 @@
 
+from datetime import datetime
+from datetime import time
 from typing import cast
 
+from dateutil.tz import tzlocal
+from dateutil.tz import UTC
 from pytest import mark
 from tinydb import TinyDB
 from tinydb.storages import MemoryStorage
 
+from tracs.activity import Resource
+from tracs.activity_types import ActivityTypes
 from tracs.db import document_cls
 from tracs.db_middleware import DataClassMiddleware
 from tracs.plugins.polar import PolarActivity
@@ -58,4 +64,58 @@ def test_write_data( db, json ):
 		'tags': [],
 		'time': '2012-01-07T09:40:51+00:00',
 		'type': 'xcski_classic'
+	}
+
+def test_write():
+	resource = Resource( name='one', type='gpx', status=100, path='one.gpx' )
+	polar = PolarActivity(
+		doc_id = 1,
+		calories = 2904,
+		distance = 30000.1,
+		duration = time( 3, 23, 53 ),
+		localtime = datetime( 2012, 1, 7, 10, 40, 51, tzinfo=tzlocal() ),
+		name = '03:23:53;0.0 km',
+		raw_id = 1234567890,
+		resources = [resource],
+		time = datetime( 2012, 1, 7, 9, 40, 51, tzinfo=UTC ),
+		type = ActivityTypes.xcski_classic,
+	)
+	storage = DataClassMiddleware( storage_cls=MemoryStorage )()
+	storage.transmap['activities'] = document_cls
+	memory_storage = cast( MemoryStorage, storage.storage )
+
+	data = {
+		"activities": {
+			"1": polar
+		}
+	}
+
+	storage.write( data )
+	written_data = memory_storage.memory
+
+	assert written_data == { 'activities' :
+		{
+			'1': {
+				'calories': 2904,
+				'classifier': 'polar',
+				'distance': 30000.1,
+				'duration': '03:23:53',
+				'groups': {},
+				'localtime': '2012-01-07T10:40:51+01:00',
+				'metadata': {},
+				'name': '03:23:53;0.0 km',
+				'raw_id': 1234567890,
+				'resources': [
+					{
+						'name': 'one',
+						'path': 'one.gpx',
+						'status': 100,
+						'type': 'gpx'
+					}
+				],
+				'tags': [],
+				'time': '2012-01-07T09:40:51+00:00',
+				'type': 'xcski_classic'
+			}
+		}
 	}

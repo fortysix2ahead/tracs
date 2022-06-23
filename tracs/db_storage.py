7d365db2fef64b3eaaa66fe285c9eebfb30bef28
fortysix2ahead <fortysix2ahead@gmail.com>
2022-06-23 10:20:23 +0200

    Finalized db storage including test cases

--- tracs/db_storage.py
+++ tracs/db_storage.py
@@ -8,8 +8,6 @@ from typing import Any
 from typing import Callable
 from typing import Dict
 from typing import Optional
-from typing import Type
-from typing import Union
 
 from orjson import loads as load_json
 from orjson import dumps as dump_as_json
@@ -34,10 +32,11 @@ class DataClassStorage( Storage ):
 	#options = OPT_APPEND_NEWLINE | OPT_INDENT_2 | OPT_SORT_KEYS| OPT_PASSTHROUGH_SUBCLASS
 	orjson_options = OPT_APPEND_NEWLINE | OPT_INDENT_2 | OPT_SORT_KEYS
 
-	def __init__( self, path: Path=None, use_memory_storage: bool=False, access_mode: bool = 'r+', cache: bool=False, passthrough=False, factory=None, *args, **kwargs ):
+	def __init__( self, path: Path=None, use_memory_storage: bool=False, access_mode: bool = 'r+', cache: bool=False, cache_size: int=1000, passthrough=False, factory=None, *args, **kwargs ):
 		super().__init__()
 
-		self._init_complete = False
+		self._initial_read = True
+		self._initial_write = True # not used ...
 
 		self._path = path
 		self._access_mode = 'r' if use_memory_storage else access_mode
@@ -47,36 +46,38 @@ class DataClassStorage( Storage ):
 		self._memory = MemoryStorage()
 		self._use_memory_storage = use_memory_storage if path else True # auto-turn on memory mode when no path is provided
 
-		self._use_cache = cache
+		self._use_cache = cache if not self._use_memory_storage else True # turn on cache if in-memory mode is on
 		self._cache_hits = 0
-		self._cache_size = 1000 if self._use_cache else 0
+		self._cache_size = cache_size if self._use_cache else 0
 
 		self._factory = factory
-		self._transformation_map: Dict[str, Union[Type, Callable]] = {}
+		if not self._factory:
+			log.warning( 'data storage initialized without a data transformation function' )
+		#self._transformation_map: Dict[str, Union[Type, Callable]] = {}
 		self._remove_null_fields: bool = True  # don't write fields which do not have a value
 		self._passthrough = passthrough
 
-	def _init( self ):
-		# initialize memory if file source exists
-		if self._path:
-			self._memory.write( self._read_data() )
-
-		self._init_complete = True
-
 	def read( self ) -> Optional[Dict[str, Dict[str, Any]]]:
-		if not self._init_complete:
-			self._init()
-
-		# read data
-		if self._use_memory_storage:
-			data = self._memory.read()
+		# initial read, either because cache mode is off or it's the initial load
+		if not self._use_cache or self._initial_read:
+			data = self.read_data_from_path()
+			self._initial_read = False
 		else:
-			data = self._read_data()
+			data = self.memory.read()
 
+		# transform data
+		data = self.read_data( data ) if not self._passthrough else data
+
+		# save data in memory
+		self._memory.write( data )
+
+		# return read data
+		return data
+
+	def read_data( self, data ) -> Any:
 		if data:
 			for table_name, table_data in data.items():
 				self.read_table( table_data )
-
 		return data
 
 	def read_table( self, table_data: Dict ) -> None:
@@ -88,22 +89,33 @@ class DataClassStorage( Storage ):
 		item_cls = self._factory( item_data, item_id ) if isfunction( self._factory ) else self._factory
 		return item_cls( item_data, int( item_id ) ) if item_cls else None
 
-	def _read_data( self ) -> Any:
+	def read_data_from_path( self ) -> Any:
 		if self._path:
 			with open( self._path, self._access_mode, self._buffering, self._encoding ) as p:
 				data = p.read()
 				return load_json( data if len( data ) > 0 else EMPTY_JSON )
+		return None
 
 	def write( self, data: Dict[str, Dict[str, Any]] ) -> None:
+		# process data
+		data = self.write_data( data ) if not self._passthrough else data
+
+		# save data to memory and increase cache counter
+		self._memory.write( data )
+
+		# persist data if not in memory mode and cache hits size
+		if not self._use_memory_storage and self._cache_hits >= self._cache_size:
+			self.flush()
+			self._cache_hits = 0
+
+		# increase cache hits
+		self._cache_hits += 1
+
+	def write_data( self, data: Dict[str, Dict[str, Any]] ) -> Any:
 		if data:
 			for table_name, table_data in data.items():
 				self.write_table( table_name, table_data )
-
-		self._memory.write( data )
-		self._cache_hits += 1
-
-		if not self._use_memory_storage:
-			self.flush()
+		return data
 
 	def write_table( self, table_name: str, table_data: Dict ):
 		for item_id, item_data in dict( table_data ).items():
@@ -114,16 +126,17 @@ class DataClassStorage( Storage ):
 	def write_item( self, item_id: str, item: Any ) -> Optional:
 		item_cls = self._factory( item, item_id ) if isfunction( self._factory ) else self._factory
 		# todo: item_cls might be None and Document needs to be used
-		return as_dict( item, item_cls, modify_arg=True, remove_null_fields=True )
+		return as_dict( item, item_cls, modify_arg=True, remove_null_fields=self._remove_null_fields )
 
-	def flush( self, force=False ) -> None:
-		if force or self._cache_hits >= self._cache_size:
-			if self._path:
-				self._path.write_bytes( dump_as_json( self._memory.read(), option=self.orjson_options ) )
-				self._cache_hits = 0
+	def flush( self ) -> None:
+		data = self._memory.read()
+		if self._path and data:
+			self._path.write_bytes( dump_as_json( data, option=self.orjson_options ) )
+		else:
+			log.warning( 'no path or data' ) # todo
 
 	def close( self ) -> None:
-		self.flush( force=True )
+		self.flush()
 
 	@property
 	def factory( self ) -> Callable:

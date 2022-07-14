1aef8b868f1b8759c737e0a79d86877f0d0d95d5
fortysix2ahead <fortysix2ahead@gmail.com>
2022-07-14 11:56:08 +0200

    Test case fixing
    Signed-off-by: fortysix2ahead <fortysix2ahead@gmail.com>

--- test/test_polar_activity.py
+++ test/test_polar_activity.py
@@ -2,28 +2,18 @@
 from datetime import datetime
 from datetime import time
 from datetime import timezone
-from pathlib import Path
 
 from dateutil.tz import tzlocal
 
 from pytest import mark
 
-from tracs.plugins.handlers import JSONHandler
 from tracs.plugins.polar import PolarActivity
 from tracs.activity_types import ActivityTypes
 
-from .helpers import get_file_path
-
-@mark.db( template='polar' )
-def test_init_from_db( json ):
-	pa = PolarActivity( json['_default']['2'], 2 )
-	assert pa.parent_id == 1
-	assert pa.parent_uid == 'group:1'
-	# assert pa.parent_ref == ActivityRef( 1, 'group:1' )
-
-	pa = PolarActivity( json['_default']['11'], 11 )
-
-	assert pa.raw_id == 1001
+@mark.file( 'libraries/default/polar/1/0/0/100001/100001.raw.json' )
+def test_init_from_raw( json ):
+	pa = PolarActivity( raw=json )
+	assert pa.raw_id == 100001
 	assert pa.name == '00:25:34;0.0 km'
 	assert pa.type == ActivityTypes.run
 	assert pa.time == datetime( 2011, 4, 28, 15, 48, 10, tzinfo=timezone.utc )
@@ -32,29 +22,20 @@ def test_init_from_db( json ):
 	assert pa.duration == time( 0, 25, 35 )
 	assert pa.calories == 456
 
-@mark.db( template='polar' )
+@mark.file( 'libraries/default/polar/1/0/0/100012/100012.raw.json' )
 def test_fitnessdata( json ):
-	json = json['_default']['12']
-	pa = PolarActivity( json, 12 )
+	pa = PolarActivity( raw=json )
+	assert pa.id == 0
+	assert pa.raw_id == 100012
 
-	assert pa.id == 12
-	assert pa.raw_id == 2002
-
-@mark.db( template='polar' )
+@mark.file( 'libraries/default/polar/1/0/0/100013/100013.raw.json' )
 def test_orthostatic( json ):
-	json = json['_default']['13']
-	pa = PolarActivity( json, 13 )
+	pa = PolarActivity( raw=json )
+	assert pa.id == 0
+	assert pa.raw_id == 100013
 
-	assert pa['id'] == 13
-	assert pa.id == 13
-	assert pa.raw_id == 3003
-
-@mark.db( template='polar' )
+@mark.file( 'libraries/default/polar/1/0/0/100014/100014.raw.json' )
 def test_rrrecording( json ):
-	json = json['_default']['14']
-	pa = PolarActivity( json, 14 )
-
-	# test id
-	assert pa['id'] == 14
-	assert pa.id == 14
-	assert pa.raw_id == 4004
+	pa = PolarActivity( raw=json )
+	assert pa.id == 0
+	assert pa.raw_id == 100014

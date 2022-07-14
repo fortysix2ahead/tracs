fc6c7cbfb207efcb5be5f04451d5236c572f3192
fortysix2ahead <fortysix2ahead@gmail.com>
2022-07-14 09:40:44 +0200

    Test case fixing
    Signed-off-by: fortysix2ahead <fortysix2ahead@gmail.com>

--- test/test_ui.py
+++ test/test_ui.py
@@ -1,12 +1,10 @@
+
 from copy import deepcopy
-
 from pytest import mark
-
 from rich.console import Console
-
 from tracs.ui import diff_table
 
-@mark.file( 'library/polar/1/0/0/100001/100001.raw.json' )
+@mark.file( 'libraries/default/polar/1/0/0/100001/100001.raw.json' )
 def test_diff_dict( json ):
 	json1, json2 = deepcopy( json ), deepcopy( json )
 	json1['country'] = 'Germany'

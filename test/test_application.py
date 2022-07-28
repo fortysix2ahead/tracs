ce4e1e6cd748046d7bfcd67c7b8dfe102cbd54dc
fortysix2ahead <fortysix2ahead@gmail.com>
2022-07-28 14:09:22 +0200

    Test case fixing.
    Signed-off-by: fortysix2ahead <fortysix2ahead@gmail.com>

--- test/test_application.py
+++ test/test_application.py
@@ -4,10 +4,12 @@ from platform import system
 
 from tracs.application import Application
 from tracs.config import ApplicationConfig as cfg
+from tracs.config import ApplicationContext
 from .helpers import get_config_path
 
 def test_app_constructor():
-	app =  Application.__new__( Application, config_dir=None, lib_dir=None, verbose=False, debug=False, force=False )
+	ctx = ApplicationContext()
+	app =  Application.__new__( Application, ctx=ctx, config_dir=None, lib_dir=None, verbose=False, debug=False, force=False )
 
 	home = Path.home()
 
@@ -30,7 +32,8 @@ def test_app_constructor():
 
 def test_app_constructor_cfg_dir():
 	env_path = get_config_path( 'debug', False )
-	app =  Application.__new__( Application, config_dir=env_path, lib_dir=None, verbose=False, debug=False, force=False )
+	ctx = ApplicationContext()
+	app =  Application.__new__( Application, ctx=ctx, config_dir=env_path, lib_dir=None, verbose=False, debug=False, force=False )
 
 	assert app.cfg_dir == Path( env_path )
 	assert app.cfg_file == Path( env_path, 'config.yaml' )
@@ -44,7 +47,8 @@ def test_app_constructor_cfg_dir():
 
 def test_app_constructor_lib_dir():
 	env_path = get_config_path( 'debug', False )
-	app =  Application.__new__( Application, config_dir=None, lib_dir=env_path, verbose=False, debug=False, force=False )
+	ctx = ApplicationContext()
+	app =  Application.__new__( Application, ctx=ctx, config_dir=None, lib_dir=env_path, verbose=False, debug=False, force=False )
 	home = Path.home()
 
 	if system() == 'Windows':
@@ -66,20 +70,21 @@ def test_app_constructor_lib_dir():
 
 def test_environment():
 	# load default/debug configuration
-	Application._instance = Application.__new__( Application, config_dir=None, lib_dir=None, verbose=False, debug=False, force=False ) # matches default object creation
+	ctx = ApplicationContext()
+	Application._instance = Application.__new__( Application, ctx=ctx, config_dir=None, lib_dir=None, verbose=False, debug=False, force=False ) # matches default object creation
 	assert cfg['debug'].get() == False
 	assert cfg['verbose'].get() == False
 	assert cfg['force'].get() == False
 
 	cfg_dir = get_config_path( 'debug', False )
 
-	Application._instance = Application.__new__( Application, config_dir=cfg_dir, lib_dir=None, verbose=None, debug=None, force=None ) # matches default object creation
+	Application._instance = Application.__new__( Application, ctx=ctx, config_dir=cfg_dir, lib_dir=None, verbose=None, debug=None, force=None ) # matches default object creation
 	assert cfg['debug'].get() == True
 	assert cfg['verbose'].get() == True
 	assert cfg['force'].get() == False
 
 	# override configuration loaded from file to simulate command line parameters
-	Application._instance = Application.__new__( Application, config_dir=cfg_dir, lib_dir=None, verbose=None, debug=None, force=True ) # matches default object creation
+	Application._instance = Application.__new__( Application, ctx=ctx, config_dir=cfg_dir, lib_dir=None, verbose=None, debug=None, force=True ) # matches default object creation
 	assert cfg['debug'].get() == True
 	assert cfg['verbose'].get() == True
 	assert cfg['force'].get() == True

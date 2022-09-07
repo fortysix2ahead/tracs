01e85cef367e778b50d85aa35a8ef9fbe39c788e
fortysix2ahead <fortysix2ahead@gmail.com>
2022-09-07 08:57:28 +0200

    Fixed rename
    Signed-off-by: fortysix2ahead <fortysix2ahead@gmail.com>

--- tracs/edit.py
+++ tracs/edit.py
@@ -8,9 +8,9 @@ from .config import GlobalConfig as gc
 log = getLogger( __name__ )
 
 def edit_activities( activities: [Activity] ):
-	pass
+	raise NotImplementedError( 'not yet implemented' )
 
-def rename_activities( activities: [Activity] ) -> None:
+def rename_activities( activities: [Activity], force: bool = False, pretend: bool = False ) -> None:
 	for a in activities:
 		answer = Prompt.ask( f'Enter new name for activity {a.id}', default=a['name'] )
 		if answer != a.name:

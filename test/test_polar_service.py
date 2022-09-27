15a5c0ff22bc2e6a6877a5be4047eef40d20ca92
fortysix2ahead <fortysix2ahead@gmail.com>
2022-09-27 16:06:01 +0200

    Updated polar service test case
    Signed-off-by: fortysix2ahead <fortysix2ahead@gmail.com>

--- test/test_polar_service.py
+++ test/test_polar_service.py
@@ -3,11 +3,10 @@ from typing import List
 
 from pytest import mark
 
-from tracs.activity import Activity
+from tracs.activity import Resource
 from tracs.config import GlobalConfig as gc
 from tracs.plugins.polar import BASE_URL
 from tracs.plugins.polar import Polar
-from tracs.plugins.polar import PolarActivity
 from .conftest import ENABLE_LIVE_TESTS
 
 from .polar_server import TEST_BASE_URL
@@ -36,35 +35,26 @@ def test_service( polar_server, service ):
 	assert service.logged_in
 
 	# fetch
-	fetched: List[Activity] = list( service._fetch() )
+	fetched: List[Resource] = list( service.fetch( False, False ) )
 
 	assert len( fetched ) == 3
-	a = fetched[0]
-	assert type( a ) is PolarActivity
-	assert a.raw is not None
-	assert a.raw_id == 300003
-	assert a.uid == 'polar:300003'
-
-	assert len( a.resources ) == 5
-
-	# download
-	for r in a.resources:
-		content, status = service.download_resource( r )
-		if r.type == 'raw':
-			assert status == 404
-		else:
-			assert content is not None and status == 200
+	r = fetched[0]
+	assert type( r ) is Resource
+	assert r.raw is not None
+	assert r.status == 200
+	assert r.uid == 'polar:300003'
 
 @mark.service( cls=Polar, base_url=TEST_BASE_URL, config='test/configurations/default/config.yaml', state='test/configurations/default/state.yaml' )
 @mark.db( template='empty', inmemory=True, update_gc=True )
 def test_workflow( polar_server, service, db ):
 	service.login()
-	fetched = service.fetch( True )
+	fetched = service.fetch( True, False )
 	assert len( fetched ) == 3
 
 @mark.skipif( not getenv( ENABLE_LIVE_TESTS ), reason='live test not enabled' )
 @mark.service( cls=Polar, url=BASE_URL, config='var/config_live.yaml', state='var/state_live.yaml' )
 @mark.db( template='empty', inmemory=True, update_gc=True )
+@mark.db_inmemory( True )
 def test_live_workflow( service, db, config_state ):
 	gc.db = db
 	gc.db_dir = db.path.parent

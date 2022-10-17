1c8add2754414c904cb609ca69f4217ca54015ea
fortysix2ahead <fortysix2ahead@gmail.com>
2022-10-17 10:13:34 +0200

    First live test for bikecitizens
    Signed-off-by: fortysix2ahead <fortysix2ahead@gmail.com>

--- test/test_bikecitizens_service.py
+++ test/test_bikecitizens_service.py
@@ -1,20 +1,13 @@
 
-from os import getenv
-
 from pytest import mark
-from pytest import skip
 
-from tracs.config import GlobalConfig as gc
 from tracs.plugins.bikecitizens import Bikecitizens
 from tracs.plugins.bikecitizens import BASE_URL
 from tracs.plugins.bikecitizens import API_URL
 
-from .conftest import ENABLE_LIVE_TESTS
 from .bikecitizens_server import TEST_API_URL
 from .bikecitizens_server import TEST_BASE_URL
-
-def test_live() -> bool:
-	return True
+from .helpers import skip_live
 
 def test_constructor():
 	bikecitizens = Bikecitizens()
@@ -32,21 +25,12 @@ def test_constructor():
 	assert bikecitizens._signin_url == f'{TEST_BASE_URL}/users/sign_in'
 	assert bikecitizens._user_url == f'{TEST_BASE_URL}/api/v1/users/None'
 
-@mark.skipif( not getenv( ENABLE_LIVE_TESTS ), reason='live test not enabled' )
-@mark.service( (Bikecitizens, BASE_URL) )
-@mark.service_config( ('var/config_live.yaml', 'var/state_live.yaml' ) )
-@mark.db_inmemory( True )
-def test_live_workflow( service, db, config_state ):
-	gc.db = db
-	gc.db_dir = db.path.parent
-	gc.db_file = db.path
-
+@skip_live
+@mark.context( library='empty', config='live', cleanup=True )
+@mark.service( cls=Bikecitizens, url=BASE_URL )
+def test_live_workflow( service ):
 	service.login()
 	assert service.logged_in
 
-	fetched = service.fetch( False )
-	assert len( fetched ) > 0
-
-	limit = 1 # don't download everything
-	for i in range( limit ):
-		service.download( fetched[i], force=True, pretend=False )
+	fetched = list( service.fetch( False, False ) )
+	assert len( fetched ) == 0

8dd6cbec96d08fc2ecefa2af07c7bff58597ecd2
fortysix2ahead <fortysix2ahead@gmail.com>
2022-10-24 22:38:36 +0200

    GPX activities now calculate localtime/localtime_end
    Signed-off-by: fortysix2ahead <fortysix2ahead@gmail.com>

--- tracs/plugins/gpx.py
+++ tracs/plugins/gpx.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from typing import Any
 from typing import Optional
 
+from dateutil.tz import tzlocal
 from dateutil.tz import UTC
 from gpxpy import parse as parse_gpx
 from gpxpy.gpx import GPX
@@ -12,7 +13,6 @@ from tracs.activity import Activity
 from tracs.plugins import document
 from tracs.plugins import importer
 from tracs.plugins.handlers import ResourceHandler
-from tracs.plugins.handlers import XML_TYPE
 from tracs.utils import seconds_to_time
 
 log = getLogger( __name__ )
@@ -27,6 +27,8 @@ class GPXActivity( Activity ):
 		self.name = gpx.name
 		self.time = gpx.get_time_bounds().start_time.astimezone( UTC )
 		self.time_end = gpx.get_time_bounds().end_time.astimezone( UTC )
+		self.localtime = self.time.astimezone( tzlocal() )
+		self.localtime_end = self.time_end.astimezone( tzlocal() )
 		self.distance = round( gpx.length_2d(), 1 )
 		self.duration = seconds_to_time( gpx.get_duration() ) if gpx.get_duration() else None
 		self.raw_id = int( self.time.strftime( '%y%m%d%H%M%S' ) )

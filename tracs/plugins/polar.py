636eb40460071b91247556f0f1c8a598a00637b3
fortysix2ahead <fortysix2ahead@gmail.com>
2022-12-15 16:14:30 +0100

    Completed multipart import in polar
    Signed-off-by: fortysix2ahead <fortysix2ahead@gmail.com>

--- tracs/plugins/polar.py
+++ tracs/plugins/polar.py
@@ -29,6 +29,7 @@ from fs.errors import CreateFailed
 from fs.zipfs import ReadZipFS
 from requests_cache import CachedSession
 from rich.prompt import Prompt
+from tinydb import Query
 
 from .gpx import GPX_TYPE
 from .handlers import JSON_TYPE
@@ -393,16 +394,17 @@ class Polar( Service ):
 		resource.status = response.status_code
 		return response.content, response.status_code
 
-	def create_activities( self, resource: Resource, **kwargs ) -> List[Activity]:
-		activities = super().create_activities( resource, **kwargs )
-		if activities[0].is_multipart:
-			recordings = [ r for r in resource.resources if r.type in [GPX_TYPE, TCX_TYPE] ]
-			partlist = self.create_partlist( activities[0], recordings )
-			for rp in partlist:
-				#part = Activity( time=rp.range.start_datetime, time_end=rp.range.end_datetime, uids=[f'{activities[0].uid}#{rp.index}'] )
-				part = Activity( time=rp.range.start_datetime, time_end=rp.range.end_datetime, uid=f'{activities[0].uid}#{rp.index}' )
-				activities.append( part )
-		return activities
+	# noinspection PyMethodMayBeStatic
+	def create_additional_activities( self, summary: Resource, *resources: Resource, **kwargs ) -> List[Activity]:
+		if any( r.type in [POLAR_ZIP_GPX_TYPE, POLAR_ZIP_TCX_TYPE] for r in resources ):
+			recordings = [r for r in resources if r.type in [GPX_TYPE, TCX_TYPE]]
+			activity = self._import_session.activities.get( summary.uid )
+			partlist = self.create_partlist( activity, recordings )
+			# update the parent activity here -> that's not nice!
+			self.ctx.db.set_field( Query()['uids'] == [activity.uid], 'parts', activity.parts )
+			return [ Activity( time=rp.range.start_datetime, time_end=rp.range.end_datetime, uid=f'{summary.uid}#{rp.index}' ) for rp in partlist ]
+		else:
+			return []
 
 	# noinspection PyMethodMayBeStatic
 	def unzip_resources( self, resources: List[Resource] ) -> List[Resource]:

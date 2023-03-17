cd334128f70d970ca5c494a7b3f0db947c03cc4d
fortysix2ahead <fortysix2ahead@gmail.com>
2023-03-17 13:55:10 +0100

    Code cleanup

--- tracs/group.py
+++ tracs/group.py
@@ -1,17 +1,13 @@
 
-from dataclasses import asdict
 from dataclasses import dataclass
 from dataclasses import field
 from datetime import datetime
 from datetime import time
 from datetime import timedelta
-from itertools import chain
-from typing import Dict
 from typing import List
 from typing import Optional
 from typing import Tuple
 
-from click import echo
 from logging import getLogger
 
 from rich.prompt import Confirm
@@ -20,7 +16,6 @@ from .activity import Activity
 from .activity_types import ActivityTypes
 from .config import ApplicationContext
 from .config import KEY_GROUPS as GROUPS
-from .config import console
 from .ui import Choice
 from .ui import diff_table2
 from .utils import seconds_to_time
@@ -53,20 +48,6 @@ class ActivityGroup:
 			for r in a.resources:
 				r.__parent_activity__ = self.head
 		self.head.uids = sorted( list( set( self.head.uids ) ) )
-
-@dataclass
-class GroupResult:
-
-	time: datetime = field( default=None )
-	target: Activity = field( default=None )
-	members: List[Activity] = field( default_factory=list )
-
-@dataclass
-class Bucket:
-
-	day: int = field( default=None )
-	queue: List[Activity] = field( default_factory=list )
-	targets: List[GroupResult] = field( default_factory=list )
 
 def group_activities( ctx: ApplicationContext, activities: List[Activity], force: bool = False ) -> None:
 	groups = group_activities2( activities )
@@ -119,77 +100,6 @@ def confirm_grouping( ctx: ApplicationContext, group: ActivityGroup, force: bool
 		headline = 'Select a name for the new activity group:'
 		choices = names
 		group.head.name = Choice.ask( headline=headline, choices=choices, use_index=True, allow_free_text=True )
-
-	return answer
-
-def _group_activities( activities: List[Activity] ) -> Tuple[List[Activity], List[Activity]]:
-	log.debug( f'attempting to group {len( activities )} activities' )
-
-	changes: List[Activity] = []
-	removals: List[Activity] = []
-	buckets: Dict[int, Bucket] = {}
-
-	for a in activities:
-		day = int( a.time.strftime( '%y%m%d' ) )
-		if not day in buckets:
-			buckets[day] = Bucket( day=day )
-		buckets[day].queue.append( a )
-
-	log.debug( f'sorted activities into {len( buckets.keys() )} buckets, based on day of activity' )
-
-	for key, bucket in buckets.items():
-		log.debug( f'analysing bucket {key}' )
-		if len( bucket.queue ) <= 1: # skip days with only one activity -> there's nothing to group
-			continue
-
-		for src in bucket.queue: # src means activity to be merged into a group
-			# check if we find a matching target group for the ungrouped activity ua
-			target = None  # target group
-			for t in bucket.targets:
-				delta_result, delta_time = _delta( t.time, src.time )
-				if delta_result:
-					target = t
-					break
-
-			# create new target group when nothing is found
-			if target:
-				target.members.append( src )
-			else:
-				bucket.targets.append( GroupResult( time=src.time, members=[ src ] ) )
-
-	# now perform the actual (interactive) grouping
-	for key, bucket in buckets.items():
-		for t in bucket.targets:
-			if len( t.members ) > 0:
-				t.target = Activity()
-				for m in t.members:
-					t.target.init_from( m )
-					t.target.uids.extend( m.uids )
-
-				if force or _confirm_grouping( t ):
-					changes.append( t.members[0] )
-					for m in t.members[1:]:
-						t.members[0].init_from( m )
-						t.members[0].uids.extend( m.uids )
-						removals.append( m )
-
-	if not pretend:
-		for c in changes: ctx.db.update( c )
-		for r in removals: ctx.db.remove( r )
-
-def _confirm_grouping( gr: GroupResult ) -> bool:
-	echo( f"Attempting to group activities:" )
-
-	table = diff_table2( result = asdict( gr.target ), sources = [ asdict( m ) for m in gr.members] )
-	console.print( table )
-
-	answer = Confirm.ask( f'Continue grouping?' )
-
-	names = list( set( [member.name for member in [gr.target] + gr.members ] ) )
-	if answer and len( names ) > 1:
-		headline = 'Select a name for the new activity group:'
-		choices = names
-		gr.members[0].name = Choice.ask( headline=headline, choices=choices, use_index=True, allow_free_text=True )
 
 	return answer
 

c912c04d7405a1b7c816aec40186ec26fbf984d9
fortysix2ahead <fortysix2ahead@gmail.com>
2023-03-20 21:34:12 +0100

    Test case fixing in test_activity
    Signed-off-by: fortysix2ahead <fortysix2ahead@gmail.com>

--- test/test_activity.py
+++ test/test_activity.py
@@ -15,34 +15,6 @@ from tracs.resources import UID
 from tracs.resources import Resource
 from tracs.activity_types import ActivityTypes
 
-@mark.file( 'libraries/default/activities.json' )
-def test_init( json ):
-	# empty init
-	a = Activity()
-	assert a['id'] == 0 and a.id == 0
-
-	a = Activity( doc_id=1 )
-	assert a['id'] == 1 and a.id == 1
-
-	a = Activity( {'name': 'Run'}, 1 )
-	assert a['id'] == 1 and a.id == 1
-	assert a.name == 'Run'
-
-	# init from db document
-	a = Activity( json['_default']['1'], 1 )
-
-	assert a.doc_id == 1 and a['doc_id'] == 1
-	assert a['id'] == 1 and a.id == 1
-	# assert a.type == "run"
-	assert a.type == ActivityTypes.run
-
-	# times
-	# assert a.time == '2012-10-24T23:29:40+00:00'
-	assert a.time == datetime( 2012, 10, 24, 23, 29, 40, tzinfo=UTC )
-	# assert a.localtime == '2012-10-24T22:29:40+01:00'
-	# assert a.localtime == datetime( 2012, 10, 24, 22, 29, 40, tzinfo=tzlocal() ) # comparison with tzlocal worked before ...
-	assert a.localtime == datetime( 2012, 10, 24, 22, 29, 40, tzinfo=timezone( timedelta( seconds=3600 ) ) )
-
 @mark.file( 'libraries/default/polar/1/0/0/100001/100001.json' )
 def test_union( json ):
 	src1 = Activity( id=1, name='One' )
@@ -78,49 +50,6 @@ def test_add():
 	assert target.duration_moving is None
 	assert target.heartrate_max == 180
 	assert target.heartrate_min == 80
-
-def test_asdict():
-	a = Activity()
-	assert a.asdict() == {
-		'timezone' : get_localzone_name(),
-	}
-
-	assert as_dict( a ) == {
-		'timezone' : get_localzone_name(),
-	}
-
-	assert as_dict( a, remove_persist=False ) == {
-		'dirty'    : False,
-		'doc_id'   : 0,
-		'id'       : 0,
-		'timezone' : get_localzone_name(),
-	}
-
-	activity_dict = {
-		'classifier': 'polar',
-		'doc_id'    : 1,
-		'id'        : 1,
-		'metadata'  : {},
-		'name'      : 'name',
-		'raw_id'    : 1,
-		'resources' : [Resource( name='one', path='one.gpx', status=100, type='gpx' )],
-		'time'      : datetime( 2020, 1, 1, 10, 0, 0, tzinfo=UTC ),
-		'type'      : ActivityTypes.run,
-		'uid'       : 'test:1'
-	}
-
-	assert as_dict( Activity( data=activity_dict ) ) == {
-		'classifier': 'polar',
-		'name'      : 'name',
-		'raw_id'    : 1,
-		'time'      : datetime( 2020, 1, 1, 10, 0, 0, tzinfo=UTC ),
-		'timezone'  : get_localzone_name(),
-		'type'      : ActivityTypes.run,
-		'uid'       : 'test:1',
-	}
-
-def test_activity_parts():
-	activity = Activity()
 
 def test_uid():
 	uid = UID( 'polar' )

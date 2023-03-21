ed4bdcbf255046c4f844fa0f74b5ac6c9bf1d38b
fortysix2ahead <fortysix2ahead@gmail.com>
2023-03-21 13:41:09 +0100

    Fixed test case for strava activity
    Signed-off-by: fortysix2ahead <fortysix2ahead@gmail.com>

--- test/test_strava_activity.py
+++ test/test_strava_activity.py
@@ -2,23 +2,24 @@
 from datetime import datetime
 from datetime import time
 from datetime import timezone
+from typing import Dict
 
+from dateutil.tz import tzlocal
 from pytest import mark
 
 from tracs.activity_types import ActivityTypes
 from tracs.plugins.strava import StravaActivity
 
-@mark.file( 'libraries/default/strava/2/0/0/200002/200002.raw.json' )
-def test_init_from_raw( json ):
-	sa = StravaActivity( raw = json )
+@mark.file( 'libraries/default/strava/2/0/0/200002/200002.json' )
+def test_init_from_raw( json: Dict ):
+	sa = StravaActivity( **json ).as_activity()
 
 	assert sa.id == 0
-	assert sa.classifier == 'strava'
-	assert sa.uid == 'strava:200002'
-	assert sa.raw_id == 200002
+	assert sa.classifiers == ['strava']
+	assert sa.uids == ['strava:200002']
 	assert sa.type == ActivityTypes.run
 	assert sa.time == datetime( 2018, 12, 16, 13, 15, 12, tzinfo=timezone.utc )
-	assert sa.localtime == datetime( 2018, 12, 16, 14, 15, 12, tzinfo=timezone.utc )
+	assert sa.localtime == datetime( 2018, 12, 16, 14, 15, 12, tzinfo=tzlocal() )
 	assert sa.distance == 8533.7
 	assert sa.speed == 2.353
 	assert sa.speed_max == 3.1
@@ -28,7 +29,7 @@ def test_init_from_raw( json ):
 	assert sa.elevation_min == 202.4
 	assert sa.duration == time( 0, 36, 25 )
 	assert sa.duration_moving == time( 0, 33, 29 )
-	assert sa.heartrate == 149.1
+	assert sa.heartrate == 149
 	assert sa.heartrate_min is None
-	assert sa.heartrate_max == 171.0
+	assert sa.heartrate_max == 171
 	assert sa.location_country == 'Germany'

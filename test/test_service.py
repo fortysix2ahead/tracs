5f676c4604ec70924b74c447f5d7d4e9eaedca02
fortysix2ahead <fortysix2ahead@gmail.com>
2023-04-04 13:19:41 +0200

    Little work on mock service

--- test/test_service.py
+++ test/test_service.py
@@ -1,22 +1,25 @@
+
 from pathlib import Path
 from typing import cast
 
 from pytest import mark
 
 from test.helpers import Mock
-from tracs.activity import Activity
+from test.helpers import MockActivity
 from tracs.db import ActivityDb
 from tracs.registry import Registry
 from tracs.resources import Resource
 from tracs.resources import ResourceType
 from tracs.service import Service
 
+from test.helpers import MOCK_TYPE
+
 @mark.context( config='empty', library='empty', cleanup=False )
 @mark.service( cls=Mock )
 def test_fetch( service ):
 	db = cast( ActivityDb, service.ctx.db )
 	service.import_activities( skip_download=True, skip_link=True )
-	assert len( db.resources.all() ) == 3
+	assert len( db.activities ) == 3
 
 	p = Path( service.ctx.db_dir_for( service.name ), '1/0/0/1001/1001.json' )
 	assert p.exists() and not Path( service.ctx.db_dir_for( service.name ), '1/0/0/1001/1001.gpx' ).exists()
@@ -37,12 +40,12 @@ def test_fetch( service ):
 @mark.service( cls=Mock )
 def test_download( service ):
 	db = cast( ActivityDb, service.ctx.db )
-	Registry.resource_types['application/mock+json'] = ResourceType( type='application/mock+json', activity_cls=Activity ) # register mock type manually
+	Registry.resource_types[MOCK_TYPE] = ResourceType( type=MOCK_TYPE, activity_cls=MockActivity ) # register mock type manually
 
 	service.import_activities( skip_download=False, skip_link=True )
 
-	assert len( db.resources.all() ) == 6
-	assert len( db.activities.all() ) == 3
+	assert len( db.resources ) == 6
+	assert len( db.activities ) == 3
 
 	p = Path( service.ctx.db_dir_for( service.name ), '1/0/0/1001/1001.gpx' )
 	assert p.exists()

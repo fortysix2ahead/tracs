755b036d075ac01805d1e00950ca5be0905e4bd0
fortysix2ahead <fortysix2ahead@gmail.com>
2023-04-06 11:21:34 +0200

    Nicer assert calls in test_cli2

--- test/test_cli2.py
+++ test/test_cli2.py
@@ -1,12 +1,39 @@
 
+from dataclasses import dataclass
+from dataclasses import field
 from typing import List
-from typing import Tuple
 
 from click.testing import CliRunner
 from pytest import mark
 
 from tracs.cli import cli
 from tracs.config import ApplicationContext
+
+@dataclass
+class Invocation:
+
+	code: int = field()
+	out: List[str] = field()
+	err: List[str] = field()
+
+	# certain line equals tern
+
+	def assert_err_line_is( self, term: str, line_no: int ):
+		assert len( self.err ) >= line_no + 1
+		assert self.err[line_no] == term
+
+	def assert_out_line_is( self, term: str, line_no: int ):
+		assert len( self.out ) >= line_no + 1
+		assert self.out[line_no] == term
+
+	def assert_term_in_err_line( self, term: str, line_no: int ):
+		assert self.err and len( self.err ) > line_no + 1
+		assert term in self.err[line_no]
+
+	def assert_term_in_stdout( self, term ):
+		assert len( self.out ) > 0
+		assert any( term in l for l in self.out )
+
 
 cmd_list = 'list'
 cmd_version = 'version'
@@ -15,25 +42,22 @@ cmd_version = 'version'
 
 @mark.context( library='default', config='default', takeout='default', cleanup=True )
 def test_nocommand( ctx ):
-	code, out, err = invoke2( ctx, '' )
-	assert code != 0 and 'Usage' in err[0]
+	invoke( ctx, '' ).assert_term_in_err_line( 'Usage', 0 )
 
 # list
 
-@mark.context( library='default', config='default', takeout='default', cleanup=False )
+@mark.context( library='default', config='default', takeout='default', cleanup=True )
 def test_list( ctx ):
-	code, out, err = invoke2( ctx, cmd_list )
-	assert code == 0 and len( out ) > 20 and 'Berlin' in out[6] # should be a table with more than 20 lines
+	invoke( ctx, cmd_list ).assert_term_in_stdout( 'Berlin' )
 
 # version
 
 @mark.context( library='default', config='default', takeout='default', cleanup=True )
 def test_version( ctx ):
-	code, out, err = invoke2( ctx, cmd_version )
-	assert code == 0 and '0.1.0' == out[0]
+	invoke( ctx, cmd_version ).assert_out_line_is( '0.1.0', 0 )
 
-def invoke2( ctx: ApplicationContext, cmdline: str ) -> Tuple[int, List[str], List[str]]:
+def invoke( ctx: ApplicationContext, cmdline: str ) -> Invocation:
 	runner = CliRunner( mix_stderr=False )
 	cmdline = f'-c {ctx.config_dir} {cmdline}'
 	result = runner.invoke( cli, cmdline )
-	return result.exit_code, result.stdout.splitlines(), result.stderr.splitlines()
+	return Invocation( result.exit_code, result.stdout.splitlines(), result.stderr.splitlines() )

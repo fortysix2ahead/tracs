99d3d9125e8d6ecade8cf95541345e9c896dca52
fortysix2ahead <fortysix2ahead@gmail.com>
2023-04-08 11:24:25 +0200

    Completed waze plugin with additional test data, fixes #73, fixes #74

--- test/test_waze_activity.py
+++ test/test_waze_activity.py
@@ -1,45 +1,57 @@
-
-from datetime import datetime
-from datetime import timezone
 from typing import cast
-
-from dateutil.tz import gettz
 
 from pytest import mark
 
-from tracs.activity_types import ActivityTypes
-from tracs.registry import Registry
-from tracs.plugins.waze import WAZE_TAKEOUT_TYPE
-from tracs.plugins.waze import WAZE_TYPE
+from tracs.plugins.waze import AccountActivity
 from tracs.plugins.waze import Waze
-from tracs.plugins.waze import WazeActivity
+from tracs.plugins.waze import WAZE_ACCOUNT_ACTIVITY_TYPE
+from tracs.plugins.waze import WAZE_ACCOUNT_INFO_TYPE
+from tracs.plugins.waze import WAZE_TYPE
 from tracs.plugins.waze import WazeImporter
+from tracs.registry import Registry
 
-from .helpers import get_file_path
+@mark.file( 'takeouts/waze/waze/2020-09/account_activity_3.csv' )
+def test_read_account_activity_2020( path ):
+	resource = Registry.importer_for( WAZE_ACCOUNT_ACTIVITY_TYPE ).load( path=path )
+	location_details = cast( AccountActivity, resource.raw ).location_details
+	assert len( location_details ) == 1
+	assert len( location_details[0].as_point_list() ) == 25
 
-@mark.file( 'templates/waze/200712_074743.json' )
-def test_read_drive( path ):
-	resource = Registry.importer_for( WAZE_TYPE ).load( path=path )
-	assert len( resource.raw ) == 137
+@mark.file( 'takeouts/waze/waze/2022-01/account_activity_3.csv' )
+def test_read_account_activity_2022( path ):
+	resource = Registry.importer_for( WAZE_ACCOUNT_ACTIVITY_TYPE ).load( path=path )
+	location_details = cast( AccountActivity, resource.raw ).location_details
+	assert len( location_details ) == 2
+	assert len( location_details[0].as_point_list() ) == 310
+	assert len( location_details[1].as_point_list() ) == 316
 
-	s = path.read_text( 'UTF-8' )
-	resource = Registry.importer_for( WAZE_TYPE ).load( from_string=s )
-	assert len( resource.raw ) == 137
+@mark.file( 'takeouts/waze/waze/2023-04/account_activity_3.csv' )
+def test_read_account_activity_2023( path ):
+	resource = Registry.importer_for( WAZE_ACCOUNT_ACTIVITY_TYPE ).load( path=path )
+	location_details = cast( AccountActivity, resource.raw ).location_details
+	assert len( location_details ) == 2
+	assert len( location_details[0].as_point_list() ) == 146
+	assert len( location_details[1].as_point_list() ) == 71
 
-@mark.file( 'takeouts/waze/waze/2020-07/account_activity_3.csv' )
-def test_read_takeout( path ):
-	resource = Registry.importer_for( WAZE_TAKEOUT_TYPE ).load( path=path )
-	assert len( resource.resources ) == 2
-	assert all( lambda r: r.type == WAZE_TAKEOUT_TYPE for r in resource.resources )
+# dummy test case: can read, but data is not used anywhere
+@mark.file( 'takeouts/waze/waze/2023-04/account_info.csv' )
+def test_read_account_info( path ):
+	resource = Registry.importer_for( WAZE_ACCOUNT_INFO_TYPE ).load( path=path )
 
-@mark.file( 'libraries/default/waze/20/07/12/200712074743/200712074743.raw.txt' )
+@mark.file( 'libraries/default/waze/20/07/12/200712074743/200712074743.txt' )
 def test_activity_from_raw( path ):
 	importer = cast( WazeImporter, Registry.importer_for( WAZE_TYPE ) )
-	points = importer.read_drive( path.read_bytes().decode('UTF-8') )
-	assert len( points ) == 137
+	resource = importer.load( path )
+	assert len( resource.raw ) == 137
 
 @mark.context( library='default', config='default', takeout='waze', cleanup=False )
 @mark.service( cls=Waze )
-def test_fetch( service ):
+def test_fetch_default( service ):
 	resources = service.fetch( force=False, pretend=False )
-	assert len( resources ) == 2
+	assert len( resources ) == 0
+
+@mark.context( library='default', config='default', takeout='waze', cleanup=False )
+@mark.service( cls=Waze )
+def test_fetch_from_takeouts( service ):
+	resources = service.fetch( force=False, pretend=False, from_takeouts=True )
+	assert len( resources ) == 4

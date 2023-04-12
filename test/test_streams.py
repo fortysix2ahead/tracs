d4490dbccfdfdfe0fdda9e9c96d22e58e30ed395
fortysix2ahead <fortysix2ahead@gmail.com>
2023-04-12 13:14:57 +0200

    GPX back and forth

--- test/test_streams.py
+++ test/test_streams.py
@@ -10,4 +10,8 @@ def test_gpx_importer( path ):
 	stream = Stream( gpx=resource.raw )
 
 	assert stream.length == 206
-	
+
+	gpx = stream.as_gpx()
+	assert len( gpx.tracks ) == 1
+	assert len( gpx.tracks[0].segments ) == 1
+	assert len( gpx.tracks[0].segments[0].points ) == 206

b1d24cc1b6cf1a52131e35109ef051732f1fede8
fortysix2ahead <fortysix2ahead@gmail.com>
2023-05-23 11:40:48 +0200

    Added verbose debug logging, showing time differences between log statements

--- tracs/__init__.py
+++ tracs/__init__.py
@@ -1,11 +1,30 @@
+from __future__ import annotations
+
+from datetime import datetime, timedelta
 from logging import DEBUG, FileHandler, Formatter, getLogger, INFO, WARNING
 from pathlib import Path
+from typing import Union
 
+from dateutil.tz import tzlocal
 from rich.logging import RichHandler
+from rich.text import Text
+
+APPLICATION_START_TIME = datetime.utcnow()
+APPLICATION_START_TIME_LOCAL = APPLICATION_START_TIME.astimezone( tzlocal() )
+LAST_LOG_TIME = datetime.now()
+
+def log_time_formatter( dt: Union[str, datetime] ) -> Text:
+	global LAST_LOG_TIME
+	dt_str = dt.strftime( "%H:%M:%S.%f" )
+	delta = dt - LAST_LOG_TIME
+	delta_str = f'{delta.seconds}.{str( delta.microseconds ).ljust( 6, "0" )}'
+	LAST_LOG_TIME = dt
+	return Text( f'{dt_str} +{delta_str}' )
 
 LOG_CONSOLE_DEFAULT_HANDLER = RichHandler( level=WARNING, show_time=False, show_level=False, markup=True )
 LOG_CONSOLE_VERBOSE_HANDLER = RichHandler( level=INFO, show_time=False, show_level=True, markup=True )
 LOG_CONSOLE_DEBUG_HANDLER = RichHandler( level=DEBUG, show_time=True, show_level=True, markup=True, log_time_format='%H:%M:%S' )
+LOG_CONSOLE_DEBUG_VERBOSE_HANDLER = RichHandler( level=DEBUG, show_time=True, show_level=True, markup=True, omit_repeated_times=False, log_time_format=log_time_formatter )
 # LOG_CONSOLE_DEBUG_HANDLER = RichHandler( level=DEBUG, show_time=True, show_level=True, markup=True, log_time_format='%Y-%m-%d %H:%M:%S.%f' )
 
 LOG_FILE_FORMAT = '[%(asctime)s] %(levelname)s: %(message)s'
@@ -18,10 +37,15 @@ def activate_log_handler( verbose: bool = False, debug: bool = False, log_path: 
 	log.handlers.clear()
 	if debug:
 		log.setLevel( DEBUG )
-		log.addHandler( LOG_CONSOLE_DEBUG_HANDLER )
+		if verbose:
+			log.addHandler( LOG_CONSOLE_DEBUG_VERBOSE_HANDLER )
+		else:
+			log.addHandler( LOG_CONSOLE_DEBUG_HANDLER )
+
 	elif verbose:
 		log.setLevel( INFO )
 		log.addHandler( LOG_CONSOLE_VERBOSE_HANDLER )
+
 	else:
 		log.setLevel( WARNING )
 		log.addHandler( LOG_CONSOLE_DEFAULT_HANDLER )
@@ -34,3 +58,6 @@ def activate_log_handler( verbose: bool = False, debug: bool = False, log_path: 
 
 # activate default log handler
 activate_log_handler()
+
+def runtime() -> timedelta:
+	return datetime.utcnow() - APPLICATION_START_TIME

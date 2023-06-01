9a20ffb0f521504a01e7e6bbb2629f200c8b6931
fortysix2ahead <fortysix2ahead@gmail.com>
2023-06-01 18:26:54 +0200

    Colored classifier icons

--- tracs/plugins/icons.py
+++ tracs/plugins/icons.py
@@ -6,12 +6,12 @@ from tracs.registry import virtualfield
 
 CLASSIFIER_ICONS = {
 	'_default': '\u2bbe',
-	'bikecitizens': '\u24b7',
-	'polar': '\u24c5',
-	'strava': '\u24c8',
-	'waze': '\u24cc',
+	'bikecitizens': '[bright_red]\u24b7[/bright_red]',
+	'polar': '[bright_blue]\u24c5[/bright_blue]',
+	'strava': '[orange1]\u24c8[/orange1]',
+	'waze': '[bright_cyan]\u24cc[/bright_cyan]',
 }
 
 @virtualfield
 def classifier_icons( a: Activity ) -> str:
-	return ''.join( [ CLASSIFIER_ICONS.get( c, CLASSIFIER_ICONS.get( '_default' ) ) for c in a.classifiers] )
+	return ' '.join( [ CLASSIFIER_ICONS.get( c, CLASSIFIER_ICONS.get( '_default' ) ) for c in a.classifiers] )

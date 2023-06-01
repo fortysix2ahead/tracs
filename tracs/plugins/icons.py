c9c14ffe3cf0b5dbed04164ec308e17171131897
fortysix2ahead <fortysix2ahead@gmail.com>
2023-06-01 14:52:13 +0200

    Showing icons works, but it does not look nice yet

--- tracs/plugins/icons.py
+++ tracs/plugins/icons.py
@@ -1,19 +1,17 @@
 
 from __future__ import annotations
 
-from typing import List
-
 from tracs.activity import Activity
 from tracs.registry import virtualfield
 
 CLASSIFIER_ICONS = {
-	'_default': '_',
-	'bikecitizens': 'B',
-	'polar': 'P',
-	'strava': 'S',
-	'waze': 'W',
+	'_default': '\u2bbe',
+	'bikecitizens': '\u24b7',
+	'polar': '\u24c5',
+	'strava': '\u24c8',
+	'waze': '\u24cc',
 }
 
 @virtualfield
-def classifier_icons( a: Activity ) -> List[str]:
-	return [ CLASSIFIER_ICONS.get( c, CLASSIFIER_ICONS.get( '_default' ) ) for c in a.classifiers]
+def classifier_icons( a: Activity ) -> str:
+	return ''.join( [ CLASSIFIER_ICONS.get( c, CLASSIFIER_ICONS.get( '_default' ) ) for c in a.classifiers] )

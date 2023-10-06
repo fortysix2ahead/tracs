13255b9d8bf7fb80356cfc0f7624ba88d432576c
fortysix2ahead <fortysix2ahead@gmail.com>
2023-10-06 09:40:43 +0200

    test case fixing

--- test/test_keywords.py
+++ test/test_keywords.py
@@ -1,12 +1,13 @@
 
 from __future__ import annotations
 
-from tracs.plugins.rule_extensions import KEYWORDS
 from tracs.core import Keyword
+from tracs.registry import Registry
 
 def test_keywords():
+	import tracs.plugins.rule_extensions
 	assert kw( 'morning' )() == 'hour >= 6 and hour < 11'
 	assert kw( 'thisyear' )().startswith( 'time >= d"20' )
 
 def kw( name: str ) -> Keyword:
-	return next( k for k in KEYWORDS if k.name == name )
+	return Registry.rule_keywords.get( name )

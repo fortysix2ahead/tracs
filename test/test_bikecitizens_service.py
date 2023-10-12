bc5c9898515c2fb38943d4a3bf0c046983469186
fortysix2ahead <fortysix2ahead@gmail.com>
2023-10-12 12:54:37 +0200

    Test case update

--- test/test_bikecitizens_service.py
+++ test/test_bikecitizens_service.py
@@ -49,6 +49,10 @@ def test_live_workflow( service: Service ):
 
 	service.persist_resources( downloaded, force=False, pretend=False )
 
+	fs = service.ctx.lib_fs
+	paths = list( fs.walk.files( 'db/bikecitizens' ) )
+	assert any( True if p.endswith( '8201735.gpx' ) else False for p in paths )
+
 	# re-download should result in no results
 	downloaded = []
 	for r in fetched:

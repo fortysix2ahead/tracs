a4257cb407b2355f23c7c6bec5348aae78117588
fortysix2ahead <fortysix2ahead@gmail.com>
2023-10-12 09:59:11 +0200

    Added structure hook for Union[str,UID] to io

--- tracs/io.py
+++ tracs/io.py
@@ -1,13 +1,14 @@
 from logging import getLogger
-from typing import List
+from typing import List, Union
 
 from attrs import define, field
-from cattrs.gen import make_dict_unstructure_fn, override
+from cattrs.gen import make_dict_structure_fn, make_dict_unstructure_fn, override
 from cattrs.preconf.orjson import make_converter
 from fs.base import FS
 from orjson import OPT_APPEND_NEWLINE, OPT_INDENT_2, OPT_SORT_KEYS
 
 from tracs.resources import Resource, Resources
+from tracs.uid import UID
 
 log = getLogger( __name__ )
 
@@ -23,7 +24,17 @@ SCHEMA_CONVERTER = make_converter()
 
 # converter configuration
 
-hook = make_dict_unstructure_fn(
+# resource
+
+resource_structure_hook = make_dict_structure_fn(
+	Resource,
+	RESOURCE_CONVERTER,
+	_cattrs_forbid_extra_keys=True,
+)
+
+RESOURCE_CONVERTER.register_structure_hook( Union[str, UID], lambda obj, cls: obj ) # uid should always be a str, so return the obj untouched
+
+resource_unstructure_hook = make_dict_unstructure_fn(
 	Resource,
 	RESOURCE_CONVERTER,
 	_cattrs_omit_if_default=True,
@@ -37,14 +48,18 @@ hook = make_dict_unstructure_fn(
 	__parents__=override( omit=True ),
 	__uid__=override( omit=True ),
 )
-RESOURCE_CONVERTER.register_unstructure_hook( Resource, hook )
+
+RESOURCE_CONVERTER.register_unstructure_hook( Resource, resource_unstructure_hook )
 
 # resource handling
 
 def load_resources( fs: FS ) -> Resources:
-	resources = RESOURCE_CONVERTER.loads( fs.readbytes( RESOURCES_PATH ), List[Resource] )
-	log.debug( f'loaded {len( resources )} resource entries from {RESOURCES_NAME}' )
-	return Resources( data = resources )
+	try:
+		resources = RESOURCE_CONVERTER.loads( fs.readbytes( RESOURCES_PATH ), List[Resource] )
+		log.debug( f'loaded {len( resources )} resource entries from {RESOURCES_NAME}' )
+		return Resources( data = resources )
+	except RuntimeError:
+		log.error( f'error loading db', exc_info=True )
 
 def write_resources( resources: Resources, fs: FS ) -> None:
 	fs.writebytes( RESOURCES_PATH, RESOURCE_CONVERTER.dumps( resources.all( sort=True ), unstructure_as=List[Resource], option=ORJSON_OPTIONS ) )

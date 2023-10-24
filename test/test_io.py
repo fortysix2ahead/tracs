cbd277f762d3c325e0447079274e092616887257
fortysix2ahead <fortysix2ahead@gmail.com>
2023-10-24 16:08:53 +0200

    Added fixture to create a composable FS

--- test/test_io.py
+++ test/test_io.py
@@ -1,5 +1,15 @@
 from pytest import mark
 
+@mark.context( config='default', lib='default', overlay='empty', takeout='waze', var='empty', persist='disk', cleanup=True )
+def test_environment( fs ):
+	for f in fs.walk.walk( '/' ):
+		print( f )
+
+	fs.writetext( '/test.txt', 'content' )
+
+	for f in fs.walk.walk( '/' ):
+		print( f )
+
 @mark.context( config='default', library='default', cleanup=False )
 def test_load_resource( ctx ):
 	resources = ctx.db.find_resources( uid='polar:100001' )

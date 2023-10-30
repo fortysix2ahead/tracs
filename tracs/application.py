1f40254a5622a1040c3475cc8d8825b2440e6c15
fortysix2ahead <fortysix2ahead@gmail.com>
2023-10-30 09:07:58 +0100

    Changed init order for logging

--- tracs/application.py
+++ tracs/application.py
@@ -1,18 +1,10 @@
-
 from __future__ import annotations
 
 from atexit import register as register_atexit
-from logging import DEBUG
-from logging import INFO
-from logging import FileHandler
-from logging import Formatter
 from logging import getLogger
-from logging import StreamHandler
-from sys import stderr
 
-from tracs import activate_log_handler
+from tracs import setup_console_logging, setup_file_logging
 from .config import ApplicationContext
-from .config import APPNAME
 from .db import ActivityDb
 from .registry import Registry
 from .utils import UCFG
@@ -20,8 +12,7 @@ from .utils import UCFG
 log = getLogger( __name__ )
 
 class Application( object ):
-
-	_instance = None # application singleton
+	_instance = None  # application singleton
 
 	@classmethod
 	def instance( cls, *args, **kwargs ):
@@ -41,22 +32,21 @@ class Application( object ):
 
 	# 'None' as default value means value has not been provided from the outside (via command line switch)
 	def __setup__( self, *args, **kwargs ):
-		# ---- create context, based on cfg_dir ----
+		# console logging setup --
+		setup_console_logging( kwargs.get( 'verbose' ), kwargs.get( 'debug' ) )
+
+		# create context, based on cfg_dir
 		self._ctx = ApplicationContext( *args, **kwargs )
 
-		# ---- logging setup: only possible after configuration has been loaded --
-		self._setup_logging( self._ctx )
+		# file logging setup after configuration has been loaded --
+		setup_file_logging( self._ctx.verbose, self._ctx.debug, self._ctx.log_file_path )
 
 		# print CLI configuration
-		log.debug( f'triggered CLI with flags debug={ kwargs }' )
+		log.debug( f'triggered CLI with flags debug={kwargs}' )
 		log.debug( f'using config dir: {self._ctx.config_dir} and library dir: {self._ctx.lib_dir}' )
 
 		# open db from config_dir
 		self._ctx.db = ActivityDb( path=self._ctx.db_dir_path, read_only=self._ctx.pretend, enable_index=self.ctx.config['db']['index'].get() )
-
-		# load plugins
-		from tracs.registry import load as load_plugins
-		load_plugins( ctx=self.ctx )
 
 		# ---- create service instances ----
 		Registry.instantiate_services( ctx=self._ctx )
@@ -67,11 +57,6 @@ class Application( object ):
 		# ---- register cleanup functions ----
 		register_atexit( self._ctx.db.close )
 		register_atexit( self._ctx.dump_state )
-
-	# noinspection PyMethodMayBeStatic
-	def _setup_logging( self, ctx: ApplicationContext ):
-		# not much to do here, just call activate log ...
-		activate_log_handler( verbose=ctx.verbose, debug=ctx.debug, log_path=ctx.log_file_path )
 
 	# ---- context ---------------------------------------------------------------
 

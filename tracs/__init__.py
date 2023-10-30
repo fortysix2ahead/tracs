34da77e94722195561e4da5a94b616d96297f685
fortysix2ahead <fortysix2ahead@gmail.com>
2023-10-30 09:07:58 +0100

    Changed init order for logging

--- tracs/__init__.py
+++ tracs/__init__.py
@@ -13,6 +13,9 @@ APPLICATION_START_TIME = datetime.utcnow()
 APPLICATION_START_TIME_LOCAL = APPLICATION_START_TIME.astimezone( tzlocal() )
 LAST_LOG_TIME = datetime.now()
 
+LOG_FILE_FORMAT = '[%(asctime)s] %(levelname)s: %(message)s'
+LOG_FILE_DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+
 def log_time_formatter( dt: Union[str, datetime] ) -> Text:
 	global LAST_LOG_TIME
 	dt_str = dt.strftime( "%H:%M:%S.%f" )
@@ -21,43 +24,54 @@ def log_time_formatter( dt: Union[str, datetime] ) -> Text:
 	LAST_LOG_TIME = dt
 	return Text( f'{dt_str} +{delta_str}' )
 
-LOG_CONSOLE_DEFAULT_HANDLER = RichHandler( level=WARNING, show_time=False, show_level=False, markup=True )
-LOG_CONSOLE_VERBOSE_HANDLER = RichHandler( level=INFO, show_time=False, show_level=True, markup=True )
-LOG_CONSOLE_DEBUG_HANDLER = RichHandler( level=DEBUG, show_time=True, show_level=True, markup=True, log_time_format='%H:%M:%S' )
-LOG_CONSOLE_DEBUG_VERBOSE_HANDLER = RichHandler( level=DEBUG, show_time=True, show_level=True, markup=True, omit_repeated_times=False, log_time_format=log_time_formatter )
+def runtime() -> timedelta:
+	return datetime.utcnow() - APPLICATION_START_TIME
+
+DEFAULT_HANDLER = RichHandler( level=WARNING, show_time=False, show_level=False, markup=True )
+VERBOSE_HANDLER = RichHandler( level=INFO, show_time=False, show_level=True, markup=True )
+DEBUG_HANDLER = RichHandler( level=DEBUG, show_time=True, show_level=True, markup=True, log_time_format='%H:%M:%S' )
+VERBOSE_DEBUG_HANDLER = RichHandler( level=DEBUG, show_time=True, show_level=True, markup=True, omit_repeated_times=False, log_time_format=log_time_formatter )
 # LOG_CONSOLE_DEBUG_HANDLER = RichHandler( level=DEBUG, show_time=True, show_level=True, markup=True, log_time_format='%Y-%m-%d %H:%M:%S.%f' )
 
-LOG_FILE_FORMAT = '[%(asctime)s] %(levelname)s: %(message)s'
-LOG_FILE_DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+CONSOLE_HANDLER = DEFAULT_HANDLER
+FILE_HANDLER = None
 
 # create root logger 'tracs'
 log = getLogger( __name__ )
 
-def activate_log_handler( verbose: bool = False, debug: bool = False, log_path: Path = None ):
-	log.handlers.clear()
-	if debug:
-		log.setLevel( DEBUG )
-		if verbose:
-			log.addHandler( LOG_CONSOLE_DEBUG_VERBOSE_HANDLER )
-		else:
-			log.addHandler( LOG_CONSOLE_DEBUG_HANDLER )
+def setup_console_logging( verbose: bool = False, debug: bool = False ):
+	global CONSOLE_HANDLER
+	log.removeHandler( CONSOLE_HANDLER )
 
-	elif verbose:
-		log.setLevel( INFO )
-		log.addHandler( LOG_CONSOLE_VERBOSE_HANDLER )
+	if verbose:
+		if debug:
+			log.setLevel( DEBUG )
+			CONSOLE_HANDLER = VERBOSE_DEBUG_HANDLER
+		else:
+			log.setLevel( INFO )
+			CONSOLE_HANDLER = VERBOSE_HANDLER
 
 	else:
-		log.setLevel( WARNING )
-		log.addHandler( LOG_CONSOLE_DEFAULT_HANDLER )
+		if debug:
+			log.setLevel( DEBUG )
+			CONSOLE_HANDLER = DEBUG_HANDLER
+		else:
+			CONSOLE_HANDLER = DEFAULT_HANDLER
+
+	log.addHandler( CONSOLE_HANDLER )
+
+def setup_file_logging( verbose: bool = False, debug: bool = False, log_path: Path = None ):
+	global FILE_HANDLER
 
 	if log_path:
 		file_handler = FileHandler( log_path, 'a' )
 		file_handler.setFormatter( Formatter( LOG_FILE_FORMAT, LOG_FILE_DATE_FORMAT ) )
 		file_handler.setLevel( DEBUG if debug else INFO )
-		log.addHandler( file_handler )
 
-# activate default log handler
-activate_log_handler()
+		if FILE_HANDLER:
+			log.removeHandler( FILE_HANDLER )
 
-def runtime() -> timedelta:
-	return datetime.utcnow() - APPLICATION_START_TIME
+		FILE_HANDLER = file_handler
+		log.addHandler( FILE_HANDLER )
+
+setup_console_logging( verbose=False, debug=False ) # activate default console logging

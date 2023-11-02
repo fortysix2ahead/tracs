b61ba1f557795497725c26b994ad24cd55be6b55
fortysix2ahead <fortysix2ahead@gmail.com>
2023-11-02 23:58:41 +0100

    Updated test_io test case

--- test/test_io.py
+++ test/test_io.py
@@ -1,6 +1,6 @@
 from pytest import mark
 
-@mark.context( config='default', lib='default', overlay='empty', takeout='waze', var='empty', persist='disk', cleanup=True )
+@mark.context( env='default', persist='disk', cleanup=True )
 def test_environment( fs ):
 	for f in fs.walk.walk( '/' ):
 		print( f )
@@ -10,7 +10,7 @@ def test_environment( fs ):
 	for f in fs.walk.walk( '/' ):
 		print( f )
 
-@mark.context( config='default', library='default', cleanup=False )
+@mark.context( env='default', persist='clone', cleanup=True )
 def test_load_resource( ctx ):
 	resources = ctx.db.find_resources( uid='polar:100001' )
 	assert len( resources ) == 2

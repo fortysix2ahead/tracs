a6bb2ebb1c95d044a57b4ecaee59f8cb2c477a30
fortysix2ahead <fortysix2ahead@gmail.com>
2023-11-03 09:52:56 +0100

    Further test case updates, better config/state processing

--- tracs/plugin.py
+++ tracs/plugin.py
@@ -1,7 +1,7 @@
 from logging import getLogger
-from typing import Any
+from typing import Any, Tuple
 
-from confuse import Configuration, NotFoundError
+from confuse import Configuration, ConfigView, NotFoundError
 
 from tracs.config import ApplicationContext
 
@@ -10,28 +10,44 @@ log = getLogger( __name__ )
 class Plugin:
 
 	def __init__( self, *args, **kwargs ) -> None:
-		# save the current context, to be able to access the context from sub-methods
-		self._ctx: ApplicationContext = kwargs.get( 'ctx' )
-
 		# configure name and display_name: this is optional
 		self._name: str = kwargs.get( 'name', self.__class__.__name__.lower() )
 		self._display_name: str = kwargs.get( 'display_name', self.__class__.__name__ )
 
-		# configure config/state views: create empty configs if context is missing
-		try:
-			self._cfg: Configuration = self._ctx.config['plugins'][self.name]
-		except (AttributeError, NotFoundError):
-			self._cfg = Configuration( appname=self.name )
+		# save the current context, to be able to access the context from sub-methods
+		self._ctx: ApplicationContext = kwargs.get( 'ctx' )
+		if self._ctx:
+			# configure config/state views: create empty configs if context is missing
+			try:
+				self._cfg: ConfigView = self._ctx.config['plugins'][self.name]
+			except (AttributeError, NotFoundError):
+				self._cfg = Configuration( appname=self.name )
 
-		try:
-			self._state: Configuration = self._ctx.state['plugins'][self.name]
-		except (AttributeError, NotFoundError):
+			try:
+				self._state: ConfigView = self._ctx.state['plugins'][self.name]
+			except (AttributeError, NotFoundError):
+				self._state = Configuration( appname=self.name )
+
+		elif kwargs.get( '_configuration' ) and kwargs.get( '_state' ): # this is mainly for testing purposes
+			self._cfg = kwargs.get( '_configuration' )
+			self._state = kwargs.get( '_state' )
+
+		else: # fallback
+			self._cfg = Configuration( appname=self.name )
 			self._state = Configuration( appname=self.name )
 
 		# enable by default
 		self._cfg['enabled'] = kwargs.get( 'enabled', True )
 
 	# helpers for setting/getting plugin configuration/state values
+
+	@property
+	def cs( self ) -> Tuple[Configuration, Configuration]:
+		"""
+		Returns a tuple with plugin configuration and state.
+		:return: tuple with configuration and state
+		"""
+		return self._cfg, self._state
 
 	def cfg_value( self, key: str ) -> Any:
 		return self.config_value( key )

e6ce4fe9d38790426012c2faf29c0e68529d51a6
fortysix2ahead <fortysix2ahead@gmail.com>
2023-12-14 11:33:00 +0100

    Proper path id for waze and fixed fetch from takeout

--- test/test_waze.py
+++ test/test_waze.py
@@ -1,4 +1,7 @@
+from pathlib import Path
 from typing import cast
+
+from gpxpy.gpx import GPX
 
 from pytest import mark
 
@@ -37,14 +40,26 @@ def test_activity_from_raw( path ):
 	resource = WazeImporter().load( path )
 	assert len( resource.data.points ) == 137
 
-@mark.context( env='default', persist='clone', cleanup=True )
 @mark.service( cls=Waze )
-def test_fetch_default( service ):
-	resources = service.fetch( force=False, pretend=False )
-	assert len( resources ) == 0
+def test_path_for( service ):
+	assert service.path_for_id( '231201102030' ) == Path( '23/12/01/231201102030' )
+	assert service.path_for_id( '1' ) == Path( '00/00/01/000001' )
+	assert service.path_for_id( '231201102030', 'waze' ) == Path( 'waze/23/12/01/231201102030' )
+	assert service.path_for_id( '231201102030', resource_path='recording.gpx' ) == Path( '23/12/01/231201102030/recording.gpx' )
+	assert service.path_for_id( '231201102030', base_path='waze', resource_path='recording.gpx' ) == Path( 'waze/23/12/01/231201102030/recording.gpx' )
+	assert service.path_for_id( '231201102030', base_path='waze', resource_path='recording.gpx', as_path=False ) == 'waze/23/12/01/231201102030/recording.gpx'
 
 @mark.context( env='default', persist='clone', cleanup=True )
 @mark.service( cls=Waze, init=True, register=True )
 def test_fetch_from_takeouts( service ):
+	# this does not do anything because from_takeouts is not set
+	resources = service.fetch( force=False, pretend=False )
+	assert len( resources ) == 0
+
+	# set takeouts flat
 	resources = service.fetch( force=False, pretend=False, from_takeouts=True )
 	assert len( resources ) == 4
+
+	for r in resources:
+		gpx = service.download( r, force=False, pretend=False )
+		assert len( gpx ) == 1 and isinstance( gpx[0].raw, GPX )

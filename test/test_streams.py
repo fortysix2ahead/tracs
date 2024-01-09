b3bc418294cd38b171b5b83731556dac0cb4c1bf
fortysix2ahead <fortysix2ahead@gmail.com>
2024-01-09 10:58:58 +0100

    Fixed test_streams

--- test/test_streams.py
+++ test/test_streams.py
@@ -1,12 +1,11 @@
 from pytest import mark
 
-from tracs.plugins.gpx import GPX_TYPE
-from tracs.registry import Registry
+from tracs.plugins.gpx import GPXImporter
 from tracs.streams import Stream
 
 @mark.file( 'templates/gpx/mapbox.gpx' )
 def test_gpx_importer( path ):
-	resource = Registry.importer_for( GPX_TYPE ).load( path=path )
+	resource = GPXImporter().load( path=path )
 	stream = Stream( gpx=resource.raw )
 
 	assert stream.length == 206

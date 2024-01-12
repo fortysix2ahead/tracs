66ba96e9efa25644951932055e738b83630d4bb0
fortysix2ahead <fortysix2ahead@gmail.com>
2024-01-12 13:43:31 +0100

    Test case fixing

--- test/test_aio.py
+++ test/test_aio.py
@@ -4,6 +4,5 @@ from pytest import mark
 from tracs.config import ApplicationContext
 
 @mark.context( env='default', persist='clone', cleanup=True )
-def test_reimport( ctx: ApplicationContext ):
-	activities = ctx.db.activities
-	print()
+def test_reimport( env ):
+	activities = env.db.activities

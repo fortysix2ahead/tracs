d48661f0818836572dcc52f3aa59263bcfd57f2f
fortysix2ahead <fortysix2ahead@gmail.com>
2024-07-31 09:03:42 +0200

    Fixed handling of metadata in union()

--- tracs/activity.py
+++ tracs/activity.py
@@ -212,6 +212,8 @@ class Activity( VirtualFieldsBase, FormattedFieldsBase ):
 						setattr( this, f.name, sorted( list( set().union( getattr( this, f.name ), other_value ) ) ) )
 					elif f.default.factory is dict:
 						setattr( this, f.name, { **value, **other_value } )
+					elif f.default.factory is Metadata:
+						pass # ignore metadata
 					else:
 						raise RuntimeError( f'unsupported factory datatype: {f.default}' )
 

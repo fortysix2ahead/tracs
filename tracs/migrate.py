a6d32483e870b67d144822fe1d62e66801c1012c
fortysix2ahead <fortysix2ahead@gmail.com>
2024-08-01 22:36:29 +0200

    Migration to db v14

--- tracs/migrate.py
+++ tracs/migrate.py
@@ -4,19 +4,26 @@ from re import compile as rxcompile
 from sys import maxsize, modules
 from typing import List
 
+from more_itertools import first_true
+from orjson import loads, dumps
+from orjson import OPT_APPEND_NEWLINE, OPT_INDENT_2, OPT_SORT_KEYS
+
 from tracs.config import ApplicationContext
+from uid import UID
 
 log = getLogger( __name__ )
 
 FN_PREFIX = '_mdb_'
 FN_REGEX = rxcompile( f'{FN_PREFIX}[a-z_]+' )
 
+ORJSON_OPTIONS = OPT_APPEND_NEWLINE | OPT_INDENT_2 | OPT_SORT_KEYS
+
 def migrate_db( ctx: ApplicationContext, function_name: str, **kwargs ):
 	full_function_name = f'{FN_PREFIX}{function_name}'
 	try:
 		log.info( f'running db migration function {full_function_name}' )
 		getattr( modules[__name__], full_function_name )( ctx, **kwargs )
-	except RuntimeError as error:
+	except AttributeError as error:
 		ctx.console.print( f'error executing db maintenance function {full_function_name}', error )
 
 def migrate_db_functions( ctx: ApplicationContext ) -> List[str]:
@@ -31,10 +38,6 @@ def _mdb_consolidate_activity_ids( ctx: ApplicationContext, **kwargs ) -> None:
 	ctx.db.commit()
 
 def _mdb_groups( ctx: ApplicationContext, **kwargs ) -> None:
-	from orjson import loads, dumps
-	from orjson import OPT_APPEND_NEWLINE, OPT_INDENT_2, OPT_SORT_KEYS
-	ORJSON_OPTIONS = OPT_APPEND_NEWLINE | OPT_INDENT_2 | OPT_SORT_KEYS
-
 	json = loads( ctx.db_fs.readbytes( 'activities.json' ) )
 	activities = []
 	for j in json:
@@ -47,5 +50,34 @@ def _mdb_groups( ctx: ApplicationContext, **kwargs ) -> None:
 				dt = datetime.fromisoformat( j.get( 'starttime' ) )
 				j['uid'] = f'group:{dt.strftime( "%y%m%d%H%M%S" )}'
 		activities.append( j )
+
+	ctx.db_fs.writebytes( 'activities2.json', dumps( activities, option=ORJSON_OPTIONS ) )
+
+def _mdb_schema( ctx: ApplicationContext, **kwargs ) -> None:
+	activities = loads( ctx.db_fs.readbytes( 'activities.json' ) )
+	resources = loads( ctx.db_fs.readbytes( 'resources.json' ) )
+
+	for r in resources:
+		p, t, uid = r['path'], r['type'], UID( uid=r['uid'] )
+		li = str( uid.local_id )
+		if uid.classifier in ['bikecitizens', 'polar', 'strava']:
+			path = f'{uid.classifier}/{li[0]}/{li[1]}/{li[2]}/{li}/{p}'
+		elif uid.classifier in [ 'waze', 'polarpersonaltrainer', 'local' ]:
+			path = f'{uid.classifier}/{li[0:2]}/{li[2:4]}/{li[4:6]}/{li}/{p}'
+		else:
+			print( f'unhandled path {uid}' )
+
+		if a := first_true( activities, pred=lambda a: a.get( 'uid' ) == str( uid ) ):
+			a['resources'] = a.get( 'resources', [] )
+			a['resources'].append( { 'path': path, 'type': t } )
+		elif a := first_true( activities, pred=lambda a: str( uid ) in a.get( 'uids', [] ) ):
+			a['metadata'] = { 'members': a.get( 'uids' ) }
+			a['resources'] = a.get( 'resources', [] )
+			a['resources'].append( { 'path': path, 'type': t } )
+		else:
+			print( f'unhandled resource: {r}' )
+
+	for a in activities:
+		a['resources'] = sorted( a.get( 'resources' ) or [], key=lambda r: r.get( 'path' ) )
 
 	ctx.db_fs.writebytes( 'activities2.json', dumps( activities, option=ORJSON_OPTIONS ) )

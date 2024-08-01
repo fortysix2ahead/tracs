77d44ce8ebb488249b44c99546275f06ee2a023a
fortysix2ahead <fortysix2ahead@gmail.com>
2024-08-01 10:09:43 +0200

    More playing around with cattrs

--- test/test_cattrs.py
+++ test/test_cattrs.py
@@ -1,7 +1,8 @@
 from datetime import timedelta
-from typing import List
+from typing import ClassVar, List
 
 from attrs import define, field
+from cattrs import Converter
 from cattrs.gen import make_dict_unstructure_fn, override
 from cattrs.preconf.orjson import make_converter
 from pytest import mark
@@ -98,3 +99,31 @@ def test_class_one_hook():
 
 if __name__ == '__main__':
 	test_cattrs()
+
+# test class specific structuring
+
+@define
+class SpecificBase:
+
+	# configure converter
+	converter: ClassVar[Converter] = make_converter()
+	converter.register_unstructure_hook( timedelta, timedelta_to_str )
+
+	id: int = field( default=None )
+	internal_id: int = field( default=None )
+	time: timedelta = field( default=None )
+
+	def _unstructure( self, conv ):
+		return { k: v for k, v in SpecificBase.converter.unstructure( self ).items() if k not in ['internal_id'] }
+
+def test_specific_base():
+	from cattrs.strategies import use_class_methods
+
+	converter = make_converter()
+	use_class_methods( converter, "_structure", "_unstructure" )
+
+	c = SpecificBase( 10, 100, timedelta( hours=2 ) )
+	d = converter.unstructure( c )
+
+	# this fails
+	assert d == {'id': 10, 'time': '7200 sec'}

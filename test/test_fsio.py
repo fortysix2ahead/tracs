94afc4c50bf48a5e1b5aa1fd6ff4fa29d72d4963
fortysix2ahead <fortysix2ahead@gmail.com>
2024-08-02 10:13:15 +0200

    Created objects.py containing test objects

--- test/test_fsio.py
+++ test/test_fsio.py
@@ -13,45 +13,7 @@ from plugins.gpx import GPX_TYPE
 from resources import Resource, ResourceList, Resources
 from uid import UID
 
-A = Activity(
-	id=1,
-	uid='polar:101',
-	starttime=datetime( 2024, 1, 3, 10, 0, 0, tzinfo=UTC ),
-	duration=timedelta( hours=2 ),
-	type=ActivityTypes.walk,
-	location_country='de',
-	metadata=Metadata(
-		created = datetime( 2024, 1, 4, 10, 0, 0, tzinfo=UTC ),
-		modified = datetime( 2024, 1, 4, 11, 0, 0, tzinfo=UTC ),
-		favourite = True,
-		members = [ UID( 'polar:101' ), UID( 'strava:101' ) ],
-	),
-	parts=[ActivityPart( uids=['polar:222', 'polar:333'], gap=timedelta( minutes=20 ) )],
-	resources=ResourceList(
-		Resource( name = 'recording.gpx', type=GPX_TYPE, path='polar/1/2/3/1234/1234.gpx', source='https://polar.com/1234/1234.gpx' )
-	)
-)
-
-AD = {
-	'id': 1,
-	'uid': 'polar:101',
-	'type': 'walk',
-	'location_country': 'de',
-	'starttime': '2024-01-03T10:00:00+00:00',
-	'duration': '02:00:00',
-	'metadata': {
-		'created': '2024-01-04T10:00:00+00:00',
-		'modified': '2024-01-04T11:00:00+00:00',
-		'favourite': True,
-		'members': ['polar:101', 'strava:101']
-	},
-	'parts': [
-		{'gap': '00:20:00', 'uids': ['polar:222', 'polar:333']}
-	],
-	'resources': [
-		{ 'name': 'recording.gpx', 'path': 'polar/1/2/3/1234/1234.gpx', 'source': 'https://polar.com/1234/1234.gpx', 'type': 'application/gpx+xml' }
-	]
-}
+from test.objects import COMPLETE_ACTIVITY as A, COMPLETE_ACTIVITY_DICT as AD
 
 @mark.context( env='default', persist='mem' )
 def test_load_schema( dbfs ):

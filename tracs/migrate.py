b34bd11c697b63c2f776a4d1928b27a99fa74406
fortysix2ahead <fortysix2ahead@gmail.com>
2024-08-15 15:19:11 +0200

    Migration of multipart activities

--- tracs/migrate.py
+++ tracs/migrate.py
@@ -4,12 +4,10 @@ from re import compile as rxcompile
 from sys import maxsize, modules
 from typing import List
 
-from more_itertools import first_true
-from orjson import loads, dumps
-from orjson import OPT_APPEND_NEWLINE, OPT_INDENT_2, OPT_SORT_KEYS
+from orjson import dumps, loads, OPT_APPEND_NEWLINE, OPT_INDENT_2, OPT_SORT_KEYS
+from rich.pretty import pprint
 
 from tracs.config import ApplicationContext
-from tracs.uid import UID
 
 log = getLogger( __name__ )
 
@@ -20,11 +18,11 @@ ORJSON_OPTIONS = OPT_APPEND_NEWLINE | OPT_INDENT_2 | OPT_SORT_KEYS
 
 def migrate_db( ctx: ApplicationContext, function_name: str, **kwargs ):
 	full_function_name = f'{FN_PREFIX}{function_name}'
-	try:
-		log.info( f'running db migration function {full_function_name}' )
-		getattr( modules[__name__], full_function_name )( ctx, **kwargs )
-	except AttributeError as error:
-		ctx.console.print( f'error executing db maintenance function {full_function_name}', error )
+#	try:
+	log.info( f'running db migration function {full_function_name}' )
+	getattr( modules[__name__], full_function_name )( ctx, **kwargs )
+#	except AttributeError as error:
+#		ctx.console.print( f'error executing db maintenance function {full_function_name}', error )
 
 def migrate_db_functions( ctx: ApplicationContext ) -> List[str]:
 	functions = [fn for fn in dir( modules[__name__] )]
@@ -55,29 +53,41 @@ def _mdb_groups( ctx: ApplicationContext, **kwargs ) -> None:
 
 def _mdb_schema( ctx: ApplicationContext, **kwargs ) -> None:
 	activities = loads( ctx.db_fs.readbytes( 'activities.json' ) )
-	resources = loads( ctx.db_fs.readbytes( 'resources.json' ) )
+	stages = [a for a in activities if a.get( 'resources' ) == []]
+	multiparts = [a for a in activities if a.get( 'parts' )]
 
-	for r in resources:
-		p, t, uid = r['path'], r['type'], UID( uid=r['uid'] )
-		li = str( uid.local_id )
-		if uid.classifier in ['bikecitizens', 'polar', 'strava']:
-			path = f'{uid.classifier}/{li[0]}/{li[1]}/{li[2]}/{li}/{p}'
-		elif uid.classifier in [ 'waze', 'polarpersonaltrainer', 'local' ]:
-			path = f'{uid.classifier}/{li[0:2]}/{li[2:4]}/{li[4:6]}/{li}/{p}'
-		else:
-			print( f'unhandled path {uid}' )
+	for a in stages:
+		if a.get( 'uid' ).endswith( '.gpx' ) or a.get( 'uid' ).endswith( '.tcx' ):
+			a['uids'] = [ a['uid'] ]
+		for uid in a.get( 'uids' ):
+			uid, path = uid.split( '/' )
+			a['resources'].append(
+				{
+					'path': f'{ctx.registry.services["polar"].path_for_uid( uid )}/{path}',
+					'type': "application/gpx+xml" if path.endswith( '.gpx' ) else "application/tcx+xml"
+				}
+			)
+		if a['uid'].startswith( 'group:' ):
+			a['uid'] = f'polar:{datetime.fromisoformat( a["starttime"] ).strftime( "%y%m%d%H%M%S" )}'
 
-		if a := first_true( activities, pred=lambda a: a.get( 'uid' ) == str( uid ) ):
-			a['resources'] = a.get( 'resources', [] )
-			a['resources'].append( { 'path': path, 'type': t } )
-		elif a := first_true( activities, pred=lambda a: str( uid ) in a.get( 'uids', [] ) ):
-			a['metadata'] = { 'members': a.get( 'uids' ) }
-			a['resources'] = a.get( 'resources', [] )
-			a['resources'].append( { 'path': path, 'type': t } )
-		else:
-			print( f'unhandled resource: {r}' )
+		del a['uids']
 
-	for a in activities:
-		a['resources'] = sorted( a.get( 'resources' ) or [], key=lambda r: r.get( 'path' ) )
+	for a in multiparts:
+		a['resources'] = [ r for r in a['resources'] if not ( r['path'].endswith( '.gpx' ) or r['path'].endswith( '.tcx' ) ) ]
+		for p in a['parts']:
+			uid, path = p['uids'][0].split( '/' )
+			rid = f'{ctx.registry.services["polar"].path_for_uid( uid )}/{path}'
+			stage = None
+			for s in stages:
+				stage_resources = [ u['path'] for u in s.get( 'resources', [] ) ]
+				if rid in stage_resources:
+					stage = s
+					break
 
-	ctx.db_fs.writebytes( 'activities2.json', dumps( activities, option=ORJSON_OPTIONS ) )
+			if not stage:
+				pprint( f'rid not found: {rid}' )
+			else:
+				p['uid'] = stage['uid']
+				del p['uids']
+
+	ctx.db_fs.writebytes( 'activities.json', dumps( activities, option=ORJSON_OPTIONS ) )

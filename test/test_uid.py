f6d20c9f697d01b8c08fc9c05dbe8d423c9a728d
fortysix2ahead <fortysix2ahead@gmail.com>
2024-08-15 15:30:05 +0200

    Test for uid equals

--- test/test_uid.py
+++ test/test_uid.py
@@ -53,6 +53,11 @@ def test_uid():
 	assert UID( classifier='polar', local_id=101, part=1 ).uid == 'polar:101#1'
 	assert UID( classifier='polar', local_id=101, path='recording.gpx', part=1 ).uid == 'polar:101/recording.gpx#1'  # works, but does not make sense ...
 
+def test_eq():
+	uid1 = UID( 'polar:101/recording.gpx' )
+	uid2 = UID( 'polar:101/recording.gpx' )
+	assert uid1 == uid2
+
 def test_lt():
 	uid1 = UID( 'polar:101' )
 	uid2 = UID( 'polar:102' )

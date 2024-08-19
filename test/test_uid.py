31342e26f2d377ad903e21fbdcc2046c9bf6bcfd
fortysix2ahead <fortysix2ahead@gmail.com>
2024-08-19 10:01:50 +0200

    Allow overwriting fields

--- test/test_uid.py
+++ test/test_uid.py
@@ -19,18 +19,16 @@ def test_uid():
 	assert uid.uid == 'polar:101' and uid.denotes_activity()
 	assert uid.as_tuple == ('polar', 101) and uid.as_triple == ( 'polar', 101, None )
 
-	# is there still a need for those special cases?
 	# special cases for convenience: allow path parameter + allow overwriting
-	# uid = UID( 'polar:101', path='recording.gpx' )
-	# assert uid.classifier == 'polar' and uid.local_id == 101 and uid.path == 'recording.gpx'
-	# assert uid.uid == 'polar:101/recording.gpx' and uid.denotes_resource()
-	# assert uid.as_tuple == ('polar', 101) and uid.as_triple == ('polar', 101, 'recording.gpx')
+	uid = UID( 'polar:101', path='recording.gpx' )
+	assert uid.classifier == 'polar' and uid.local_id == 101 and uid.path == 'recording.gpx'
+	assert uid.uid == 'polar:101/recording.gpx' and uid.denotes_resource()
+	assert uid.as_tuple == ('polar', 101) and uid.as_triple == ('polar', 101, 'recording.gpx')
 
-	# special cases for convenience: allow overwriting
 	# uid = UID( 'polar:101/file.gpx', path='recording.gpx' )
-	# assert uid.classifier == 'polar' and uid.local_id == 101 and uid.path == 'recording.gpx'
-	# assert uid.uid == 'polar:101/recording.gpx' and uid.denotes_resource()
-	# assert uid.as_tuple == ('polar', 101) and uid.as_triple == ('polar', 101, 'recording.gpx')
+	assert uid.classifier == 'polar' and uid.local_id == 101 and uid.path == 'recording.gpx'
+	assert uid.uid == 'polar:101/recording.gpx' and uid.denotes_resource()
+	assert uid.as_tuple == ('polar', 101) and uid.as_triple == ('polar', 101, 'recording.gpx')
 
 	uid = UID( 'polar:101/recording.gpx' )
 	assert uid.classifier == 'polar' and uid.local_id == 101 and uid.path == 'recording.gpx'

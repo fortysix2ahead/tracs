073466cae9bfbf56de68f35823106407ad571e84
fortysix2ahead <fortysix2ahead@gmail.com>
2024-09-26 09:59:05 +0200

    Expanded test cases

--- test/test_local.py
+++ test/test_local.py
@@ -1,17 +1,18 @@
 
 from __future__ import annotations
 
-from pytest import mark
-
+from io import UnsupportedOperation
 from logging import getLogger
 
-from plugins.local import Local
+from pytest import mark, raises
+
+from tracs.plugins.local import Local
 
 log = getLogger( __name__ )
 
 @mark.context( env='default', persist='clone', cleanup=True )
 @mark.service( cls=Local, init=True, register=True )
-def test_unified_import( service ):
+def test_unified_import_from_zip( service ):
 	location = service.ctx.config_fs.getsyspath( 'takeouts/drivey.zip' )
 	activities, fs = service.unified_import( service.ctx, classifier='drivey', location=location )
 	assert len( activities ) == 13
@@ -20,8 +21,23 @@ def test_unified_import( service ):
 
 	assert sorted( fs.listdir( 'drivey/24/08' ) ) == ['25', '26', '27', '28']
 	assert sorted( fs.listdir( 'drivey/24/08/27/240827145524' ) ) == [ '240827145524.gpx' ]
+	assert all( [ f.endswith( '.gpx' ) for f in fs.walk.files() ] )
 
-	# same data, this time from dir
+@mark.context( env='default', persist='clone', cleanup=True )
+@mark.service( cls=Local, init=True, register=True )
+def test_unified_import_from_dir( service ):
 	location = service.ctx.config_fs.getsyspath( 'takeouts/drivey' )
 	activities, fs = service.unified_import( service.ctx, classifier='drivey', location=location )
 	assert len( activities ) == 13
+	assert all( [ f.endswith( '.gpx' ) for f in fs.walk.files() ] )
+
+@mark.context( env='default', persist='clone', cleanup=True )
+@mark.service( cls=Local, init=True, register=True )
+def test_unified_import_fail( service ):
+	with raises( UnsupportedOperation ):
+		activities, fs = service.unified_import( service.ctx, classifier='drivey', location='something_that_does_not_exist' )
+
+	# todo: this should be allowed
+	location = service.ctx.config_fs.getsyspath( 'takeouts/drivey/drive-20240825-160655.gpx' )
+	with raises( UnsupportedOperation ):
+		activities, fs = service.unified_import( service.ctx, classifier='drivey', location=location )

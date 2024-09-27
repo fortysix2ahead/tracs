ea6fa8e80e60a1bb70d2bc916dd2aac6043c1b5b
fortysix2ahead <fortysix2ahead@gmail.com>
2024-09-27 12:19:35 +0200

    Added import from single file

--- test/test_local.py
+++ test/test_local.py
@@ -10,6 +10,7 @@ from tracs.plugins.local import Local
 
 log = getLogger( __name__ )
 
+# noinspection PyUnresolvedReferences
 @mark.context( env='default', persist='clone', cleanup=True )
 @mark.service( cls=Local, init=True, register=True )
 def test_unified_import_from_zip( service ):
@@ -23,6 +24,15 @@ def test_unified_import_from_zip( service ):
 	assert sorted( fs.listdir( 'drivey/24/08/27/240827145524' ) ) == [ '240827145524.gpx' ]
 	assert all( [ f.endswith( '.gpx' ) for f in fs.walk.files() ] )
 
+	service.db._activities.add( activities[0] )
+	activities2, fs = service.unified_import( service.ctx, classifier='drivey', location=location )
+	assert len( activities2 ) == 12
+
+	activities2, fs = service.unified_import( service.ctx, classifier='drivey', location=location, force=True )
+	assert len( activities2 ) == 13
+
+# noinspection PyUnresolvedReferences
+
 @mark.context( env='default', persist='clone', cleanup=True )
 @mark.service( cls=Local, init=True, register=True )
 def test_unified_import_from_dir( service ):
@@ -31,13 +41,19 @@ def test_unified_import_from_dir( service ):
 	assert len( activities ) == 13
 	assert all( [ f.endswith( '.gpx' ) for f in fs.walk.files() ] )
 
+# noinspection PyUnresolvedReferences
+@mark.context( env='default', persist='clone', cleanup=True )
+@mark.service( cls=Local, init=True, register=True )
+def test_unified_import_from_file( service ):
+	location = service.ctx.config_fs.getsyspath( 'takeouts/drivey/drive-20240825-160655.gpx' )
+	activities, fs = service.unified_import( service.ctx, classifier='drivey', location=location )
+	assert len( activities ) == 1
+	assert all( [ f.endswith( '.gpx' ) for f in fs.walk.files() ] )
+
+# noinspection PyUnresolvedReferences
+
 @mark.context( env='default', persist='clone', cleanup=True )
 @mark.service( cls=Local, init=True, register=True )
 def test_unified_import_fail( service ):
 	with raises( UnsupportedOperation ):
 		activities, fs = service.unified_import( service.ctx, classifier='drivey', location='something_that_does_not_exist' )
-
-	# todo: this should be allowed
-	location = service.ctx.config_fs.getsyspath( 'takeouts/drivey/drive-20240825-160655.gpx' )
-	with raises( UnsupportedOperation ):
-		activities, fs = service.unified_import( service.ctx, classifier='drivey', location=location )

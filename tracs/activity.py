135b63a80a97f6284ffb8ecc2a639605f3d48e36
fortysix2ahead <fortysix2ahead@gmail.com>
2024-11-06 09:26:28 +0100

    Added support for timedelta formatting

--- tracs/activity.py
+++ tracs/activity.py
@@ -17,7 +17,7 @@ from tzlocal import get_localzone_name
 from tracs.activity_types import ActivityTypes
 from tracs.core import FormattedFieldsBase, Metadata, VirtualFieldsBase
 from tracs.resources import Resource, Resources
-from tracs.ui.utils import fmt_datetime, fmt_default
+from tracs.ui.utils import fmt_datetime, fmt_default, fmt_timedelta
 from tracs.uid import UID
 from tracs.utils import fromisoformat, str_to_timedelta, sum_timedeltas, timedelta_to_str, toisoformat, unique_sorted
 
@@ -609,3 +609,5 @@ Activity.converter.register_structure_hook( Resources, lambda obj, cls: Resource
 Activity.__fmf__['__default__'] = fmt_default
 for f in [ 'starttime', 'starttime_local', 'endtime', 'endtime_local' ]:
 	Activity.__fmf__[f] = fmt_datetime
+for f in [ 'duration', 'duration_moving' ]:
+	Activity.__fmf__[f] = fmt_timedelta
--- tracs/ui/utils.py
+++ tracs/ui/utils.py
@@ -1,20 +1,32 @@
-from datetime import datetime
-from typing import Any, List, Optional
+from datetime import datetime, timedelta
+from typing import Any, List, Literal, Optional
 
-from babel.dates import format_datetime
+from babel.dates import format_datetime, format_timedelta
 from rich.pretty import Pretty
 
 DEFAULT_LOCALE = 'en'
 
-DEFAULT_FORMAT = 'medium'
+DATETIME_FORMATS = Literal[ 'full', 'long', 'medium', 'short' ]
+TIMEDELTA_FORMATS = Literal[ 'narrow', 'short', 'medium', 'long' ]
+
+DEFAULT_DATETIME_FORMAT = 'medium'
+DEFAULT_TIMEDELTA_FORMAT = 'long'
 
 # formatting
 
 def fmt_default( obj: Any, fmt: Optional[str] = None, locale: Optional[str] = None ) -> str|Pretty:
 	return str( obj ) if obj is not None else ''
 
-def fmt_datetime( obj: datetime, fmt: Optional[str] = None, locale: Optional[str] = None ) -> str|Pretty:
-	return format_datetime( obj, fmt or DEFAULT_FORMAT, None, locale or DEFAULT_LOCALE )
+def fmt_datetime( obj: datetime, fmt: DATETIME_FORMATS = None, locale: Optional[str] = None ) -> str|Pretty:
+	fmt = fmt if fmt else DEFAULT_DATETIME_FORMAT
+	return format_datetime( obj, fmt, None, locale or DEFAULT_LOCALE )
+
+def fmt_timedelta( obj: timedelta, fmt: TIMEDELTA_FORMATS = None, locale: str = DEFAULT_LOCALE ) -> str|Pretty:
+	try:
+		fmt = fmt if fmt else DEFAULT_TIMEDELTA_FORMAT
+		return format_timedelta( obj, format=fmt, locale=locale or DEFAULT_LOCALE )
+	except TypeError:
+		return ''
 
 # styling
 

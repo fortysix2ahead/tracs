ae27240e97c182822df26d9fe84fb3a78342c56f
fortysix2ahead <fortysix2ahead@gmail.com>
2025-02-20 11:17:49 +0100

    Test case fixing

--- test/test_keywords.py
+++ test/test_keywords.py
@@ -3,20 +3,19 @@ from __future__ import annotations
 
 from logging import getLogger
 
+from tracs.pluginmgr import PluginManager
 from tracs.core import Keyword
-from tracs.registry import Registry
 
 log = getLogger( __name__ )
 
 def setup_module( module ):
 	# noinspection PyUnresolvedReferences
-	import tracs.plugins.rule_extensions
-	log.info( 'importing tracs.plugins.rule_extensions' )
+	import tracs.plugins.keywords
+	log.info( 'importing tracs.plugins.keywords' )
 
 def test_keywords():
-
 	assert kw( 'morning' )() == 'hour >= 6 and hour < 11'
 	assert kw( 'thisyear' )().startswith( 'starttime_local >= d"20' )
 
 def kw( name: str ) -> Keyword:
-	return Registry.instance().keywords.get( name )
+	return PluginManager.inst().registry()._keyword.get( name )

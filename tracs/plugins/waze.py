508aff25d65a4298012757cefa56fda9437da57d
fortysix2ahead <fortysix2ahead@gmail.com>
2025-02-27 10:41:42 +0100

    Fixed waze plugin, improved logging

--- tracs/plugins/waze.py
+++ tracs/plugins/waze.py
@@ -496,26 +496,29 @@ class Waze( Service ):
 		return any ( [ f for f in fs.walk.files( '/', filter=[ ACTIVITY_FILE ] ) ] )
 
 	def import_from_fs( self, src_fs: FS, dst_fs: FS, **kwargs ) -> Activities:
-		log.info( f'searning {src_fs} for Waze takeout files' )
+		log.info( f'searching {src_fs.getsyspath( "" )} for Waze takeout files' )
 
 		# check if activity files are already known
 		activity_files = sorted( [ f for f in src_fs.walk.files( '/', filter=[ ACTIVITY_FILE ] ) ] )
 		known_files = list( unique( [ r.source for r in self.db.resources if r.source is not None ] ) )
 		known_files = [ frombase( self.name, kf ) for kf in known_files if parts( relpath( kf ) )[1] == self.name ]
 
+		# logging
+		for kn in known_files:
+			log.debug( f'skipping import from already known takeout file {kn}' )
 		log.info( f'found {len( activity_files )} takeout file(s) from which {len( known_files )} are already known (data has already been imported from)' )
 
 		if not self.ctx.force:
 			activity_files = [ af for af in activity_files if af not in known_files ]
 
 		log.info( f'found {len( activity_files )} unknown takeout file(s) which are used for import' )
-		self.ctx.total( len( activity_files ) )
+		# self.ctx.total( len( activity_files ) )
 
 		activities = Activities()
 
 		for file in activity_files:
 			log.info( f'importing activities from Waze takeout in {file}' )
-			self.ctx.advance( f'{file}' )
+			# self.ctx.advance( f'{file}' )
 
 			takeout_resource = self._takeout_importer.load( fs=src_fs, path=file )
 			account_activity = cast( AccountActivity, takeout_resource.data )

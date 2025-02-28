bde63b276793f0610024dbfee6244a9954ad0406
fortysix2ahead <fortysix2ahead@gmail.com>
2025-02-28 07:08:50 +0100

    Test case fixing

--- test/test_application.py
+++ test/test_application.py
@@ -1,74 +1,15 @@
 from pathlib import Path
 from platform import system
 
-import platformdirs
-from fs.base import FS
-from fs.memoryfs import MemoryFS
-from fs.osfs import OSFS
-from fs.subfs import SubFS
+from fs.appfs import UserDataFS
 from pytest import mark
 
 from tracs.application import Application
-from tracs.context import ApplicationContext, current_ctx
 from tracs.constants import APPNAME
-
-@mark.context( env='empty', persist='clone', cleanup=True )
-def test_context( fs: FS ):
-	vrp = fs.getsyspath( '' )
-
-	ctx = ApplicationContext()
-	assert ctx.config_fs
-	assert ctx.config_dir == OSFS( root_path=platformdirs.user_config_dir( 'tracs' ), expand_vars=True ).getsyspath( '' )
-	assert ctx.lib_dir == OSFS( root_path=platformdirs.user_config_dir( 'tracs' ), expand_vars=True ).getsyspath( '' )
-	assert ctx.db_dir == OSFS( root_path=platformdirs.user_config_dir( 'tracs' ), expand_vars=True ).getsyspath( 'db' )
-
-	ctx = ApplicationContext( __kwargs__= { 'configuration': vrp } )
-	assert ctx.config_fs
-	assert ctx.config_dir == vrp
-	assert ctx.config_file == fs.getsyspath( '/config.yaml' )
-	assert ctx.state_file == fs.getsyspath( '/state.yaml' )
-	assert ctx.lib_dir == vrp
-	assert ctx.db_dir == f'{vrp}db'
-	assert ctx.overlay_dir == f'{vrp}overlay'
-	assert ctx.plugin_dir( 'polar' ) == f'{vrp}db/polar/'
-
-	assert ctx.takeouts_dir == f'{vrp}takeouts'
-	assert ctx.takeout_dir( 'polar' ) == f'{vrp}takeouts/polar/'
-
-	ctx = ApplicationContext( __kwargs__= { 'configuration': f'{vrp}/config.yaml' } )
-	assert ctx.config_fs
-	assert ctx.config_dir == vrp
-	assert ctx.config_file == fs.getsyspath( '/config.yaml' )
-	assert ctx.lib_dir == vrp
-	assert ctx.db_dir == f'{vrp}db'
-
-	ctx = ApplicationContext( config_fs=MemoryFS() )
-	assert type( ctx.config_fs ) is MemoryFS
-	assert ctx.config_dir is None and ctx.config_file is None
-	assert type( ctx.lib_fs ) in [MemoryFS, SubFS]
-
-	assert ctx.takeouts_fs
-
-@mark.context
-def test_default_context( ctx: ApplicationContext ):
-	# this should result in a ctx with memory as backend -> just for easy testing
-	assert type( ctx.config_fs ) is MemoryFS
-	assert type( ctx.lib_fs ) in [MemoryFS, SubFS]
-
-	assert ctx.config_fs.listdir( '/' ) != []
-	assert ctx.lib_fs.listdir( '/db' ) == []
-
-@mark.context( env='default', persist='mem' )
-def test_mem_context( ctx: ApplicationContext ):
-	# this should result in a ctx with memory as backend, with the default environment loaded
-	assert type( ctx.config_fs ) is MemoryFS
-	assert type( ctx.lib_fs ) in [MemoryFS, SubFS]
-
-	assert ctx.config_fs.listdir( '/' )
-	assert ctx.lib_fs.listdir( '/db' ) != []
+from tracs.context import current_ctx
 
 def test_app_constructor():
-	app =  Application.__new__( Application, verbose=False, debug=False, force=False )
+	app =  Application.__new__( Application, verbose=False, debug=False, force=False, json=False )
 	home = Path.home()
 
 	if system() == 'Windows':
@@ -90,7 +31,7 @@ def test_app_constructor_cfg_dir( ctx ):
 	app =  Application.__new__( Application, configuration=cfg, verbose=False, debug=False, force=False )
 
 	assert app.ctx.config_dir == f'{str( cfg_dir )}'
-	assert app.ctx.lib_dir == f'{str( cfg_dir )}'
+	assert app.ctx.lib_dir == UserDataFS( 'tracs', create=True ).getsyspath( '' )
 
 @mark.context( env='empty', persist='clone', cleanup=True )
 def test_app_constructor_lib_dir( ctx ):
@@ -110,15 +51,12 @@ def test_app_constructor_lib_dir( ctx ):
 	assert app.ctx.config_dir == f'{str( cfg_dir )}/'
 	assert app.ctx.lib_dir == f'{str( ctx.lib_dir )}'
 
+
 def test_default_environment():
 	app = Application.__new__( Application, verbose=False, debug=False, force=False ) # matches default object creation
 	assert app.ctx.debug == False
 	assert app.ctx.verbose == False
 	assert app.ctx.force == False
-
-	names = ['bikecitizens', 'local', 'polar', 'strava', 'stravaweb', 'waze']
-	# noinspection PyTestUnpassedFixture
-	assert all( n in current_ctx().service_mgr.service_names() for n in names )
 
 @mark.context( env='debug', persist='clone', cleanup=True )
 def test_debug_environment( ctx ):
--- test/test_context.py
+++ test/test_context.py
@@ -13,6 +13,7 @@ def test_context( fs: FS ):
 	std_cfg_fs = UserConfigFS( 'tracs', create=True )
 	std_lib_fs = UserDataFS( 'tracs', create=True )
 
+	# create default context, points to user config/data fs
 	ctx = ApplicationContext()
 
 	assert ctx.config_fs.getsyspath( '' ) == std_cfg_fs.getsyspath( '' )
@@ -26,10 +27,10 @@ def test_context( fs: FS ):
 	assert ctx.lib_dir == std_lib_fs.getsyspath( '' )
 	assert ctx.db_dir == std_lib_fs.getsyspath( 'db' )
 
+	# this should not fail
 	ctx._load_configuration()
 
-	# adjust fs after creation
-
+	# adjust fs after creation, use temp fs created as fixture
 	ctx.config_fs = fs
 	ctx.lib_fs = fs
 	assert ctx.config_dir == fs.getsyspath( '' )
@@ -40,8 +41,7 @@ def test_context( fs: FS ):
 	assert ctx.takeouts_dir == fs.getsyspath( 'takeouts' )
 	assert ctx.takeout_dir( 'polar' ) == fs.getsyspath( 'takeouts/polar/' )
 
-	# use strings instead of fs of configuraing config and lib fs
-
+	# use strings instead of fs of configuring config and lib fs
 	ctx.config_fs = fs.getsyspath( '' )
 	ctx.lib_fs = fs.getsyspath( '' )
 	assert ctx.config_dir == fs.getsyspath( '' )
@@ -51,9 +51,32 @@ def test_context( fs: FS ):
 	assert ctx.config_dir == fs.getsyspath( '' )
 
 	# try memory fs
-
 	ctx.config_fs = MemoryFS()
 	assert type( ctx.config_fs ) is MemoryFS
 	with raises( NoSysPath ):
 		assert ctx.config_dir
 	assert type( ctx.var_fs ) in [MemoryFS, SubFS]
+
+	# create context using kwargs
+	config_file_path = fs.getsyspath( 'config.yaml' )
+	ctx = ApplicationContext( _cli_kwargs= { 'configuration': config_file_path } )
+	assert ctx.config_dir == fs.getsyspath( '' )
+	assert ctx.config_file == config_file_path
+
+@mark.context
+def test_default_context( ctx: ApplicationContext ):
+	# this should result in a ctx with a memory backend -> just for easy testing
+	assert type( ctx.config_fs ) is MemoryFS
+	assert type( ctx.lib_fs ) in [MemoryFS, SubFS]
+
+	assert ctx.config_fs.listdir( '/' ) != []
+	assert ctx.lib_fs.listdir( '/db' ) == []
+
+@mark.context( env='default', persist='mem' )
+def test_mem_context( ctx: ApplicationContext ):
+	# this should result in a ctx with memory as backend, with the default environment loaded
+	assert type( ctx.config_fs ) is MemoryFS
+	assert type( ctx.lib_fs ) in [MemoryFS, SubFS]
+
+	assert ctx.config_fs.listdir( '/' )
+	assert ctx.lib_fs.listdir( '/db' ) != []

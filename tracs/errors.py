e550fa2f1b867613d10d858b6829237e25552eda
fortysix2ahead <fortysix2ahead@gmail.com>
2026-03-20 10:26:08 +0100

    Added ImportException

--- tracs/errors.py
+++ tracs/errors.py
@@ -1,8 +1,18 @@
 from typing import Optional
 
-class ResourceImportException( Exception ):
+class ImportException( Exception ):
+	"""Exception raised in case imports are failing.
+	"""
 
-	def __init__( self, message: str, cause: Optional[Exception] ):
+	def __init__( self, message: str, cause: Optional[Exception] = None ):
 		super().__init__()
 		self.__message__ = message
 		self.__cause__ = cause
+
+class ResourceImportException( Exception ):
+
+	def __init__( self, message: str, cause: Optional[Exception] = None ):
+		super().__init__()
+		self.__message__ = message
+		self.__cause__ = cause
+

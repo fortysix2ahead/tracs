b70d4dcdf38bcf2ba094f94f72a2d6b90d2844fc
fortysix2ahead <fortysix2ahead@gmail.com>
2026-03-31 10:26:56 +0200

    Testing: test_fsio green

--- test/test_fsio.py
+++ test/test_fsio.py
@@ -19,7 +19,12 @@ def load_from( s: str ) -> Dict:
 
 @mark.context( env='default', persist='mem' )
 def test_load_schema( dbfs ):
-	assert load_schema( dbfs ).version == 14
+	assert load_schema( dbfs ).version == 15
+
+@mark.unit
+def test_uid():
+	assert converter.unstructure( UID_OBJ ) == UID_DUMP
+	assert converter.structure( UID_DUMP, UID ) == UID_OBJ
 
 @mark.unit
 def test_metadata():
@@ -31,7 +36,8 @@ def test_resource():
 	assert dump_to_str( converter.unstructure( RESOURCE_OBJ ) ) == RESOURCE_OBJ_DUMP
 	assert converter.structure( load_from( RESOURCE_OBJ_DUMP ), Resource ) == RESOURCE_OBJ
 
-	# resources
+@mark.unit
+def test_resources():
 	assert dump_to_str( converter.unstructure( RESOURCES_OBJ ) ) == RESOURCES_OBJ_DUMP
 	assert converter.structure( load_from( RESOURCES_OBJ_DUMP ), Resources ) == RESOURCES_OBJ
 
@@ -41,16 +47,12 @@ def test_activity_type():
 	assert converter.structure( 'run', ActivityTypes ) == ActivityTypes.run
 
 @mark.unit
-def test_activity_part():
-	assert dump_to_str( converter.unstructure( ACTIVITY_PART_OBJ ) ) == ACTIVITY_PART_OBJ_DUMP
-	assert converter.structure( load_from( ACTIVITY_PART_OBJ_DUMP ), ActivityPart ) == ACTIVITY_PART_OBJ
-
-@mark.unit
 def test_activity():
 	assert dump_to_str( converter.unstructure( ACTIVITY_OBJ ) ) == ACTIVITY_OBJ_DUMP
 	assert converter.structure( load_from( ACTIVITY_OBJ_DUMP ), Activity ) == ACTIVITY_OBJ
 
-	# activities
+@mark.unit
+def test_activities():
 	assert dump_to_str( converter.unstructure( ACTIVITIES_OBJ ) ) == ACTIVITIES_OBJ_DUMP
 	assert converter.structure( load_from( ACTIVITIES_OBJ_DUMP ), Activities ) == ACTIVITIES_OBJ
 
@@ -59,30 +61,14 @@ def test_multipart_activity():
 	assert dump_to_str( converter.unstructure( MULTIPART_ACTIVITY_OBJ ) ) == MULTIPART_ACTIVITY_OBJ_DUMP
 	assert converter.structure( load_from( MULTIPART_ACTIVITY_OBJ_DUMP ), MultipartActivity ) == MULTIPART_ACTIVITY_OBJ
 
-@mark.context( env='default', persist='mem' )
+@mark.context( env='empty' )
 def test_activities( dbfs ):
-	activities = Activities()
-	activities.add( A )
+	activities = Activities( ACTIVITY_OBJ )
 
 	# write to dbfs
 	write_activities( activities, dbfs )
 
-	json = loads( dbfs.readtext( 'activities.json' ) )
-	assert json == [ AD ]
-
 	#  load again from dbfs
 	activities = load_activities( dbfs )
 
-	assert len( activities ) == 1
-	a1 = activities[0]
-	assert a1.id == 1 and a1.uid == 'polar:101'
-	assert a1.duration == timedelta( hours=2 )
-	assert a1.starttime == datetime( 2024, 1, 3, 10, 0, 0, tzinfo=UTC )
-	# assert a1.type == ActivityTypes.walk # don't know why this fails
-	assert a1.type.name == 'walk'
-
-	assert a1.parts[0].gap == timedelta( minutes=20 )
-	assert a1.parts[0].uid == UID.from_str( 'polar:222#1' )
-
-	assert a1.metadata.created == datetime( 2024, 1, 4, 10, 0, 0, tzinfo=UTC )
-	assert a1.metadata.favourite
+	assert activities == [ ACTIVITY_OBJ ]

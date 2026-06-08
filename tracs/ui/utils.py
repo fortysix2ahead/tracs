109aa290565def4c08958fabd209665aec1bf8b7
fortysix2ahead <fortysix2ahead@gmail.com>
2026-06-08 11:52:02 +0200

    Added Optional type hints in ui.utils

--- tracs/ui/utils.py
+++ tracs/ui/utils.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-from typing import Any, List, Literal
+from typing import Any, List, Literal, Optional
 
 from babel.dates import format_datetime, format_timedelta
 from babel.numbers import format_decimal
@@ -15,20 +15,20 @@ DEFAULT_TIMEDELTA_FORMAT = 'long'
 
 # formatting
 
-def fmt_default( obj: Any, fmt: str = None, locale: str = None ) -> str|Pretty:
+def fmt_default( obj: Any, fmt: Optional[str] = None, locale: Optional[str] = None ) -> str|Pretty:
 	return str( obj ) if obj is not None else ''
 
-def fmt_decimal( obj: float, fmt: str = None, locale: str = None ) -> str|Pretty:
+def fmt_decimal( obj: float, fmt: Optional[str] = None, locale: Optional[str] = None ) -> str|Pretty:
 	fmt = fmt or '#,###.#'
 	locale = locale or DEFAULT_LOCALE
 	return '' if obj is None else format_decimal( obj, format=fmt, locale=locale )
 
-def fmt_datetime( obj: datetime, fmt: DATETIME_FORMATS = None, locale: str = None ) -> str|Pretty:
+def fmt_datetime( obj: datetime, fmt: Optional[DATETIME_FORMATS] = None, locale: Optional[str] = None ) -> str|Pretty:
 	fmt = fmt or DEFAULT_DATETIME_FORMAT
 	locale = locale or DEFAULT_LOCALE
 	return format_datetime( obj, fmt, None, locale )
 
-def fmt_timedelta( obj: timedelta, fmt: TIMEDELTA_FORMATS = None, locale: str = None ) -> str|Pretty:
+def fmt_timedelta( obj: timedelta, fmt: Optional[TIMEDELTA_FORMATS] = None, locale: Optional[str] = None ) -> str|Pretty:
 	try:
 		fmt = fmt or DEFAULT_TIMEDELTA_FORMAT
 		locale = locale or DEFAULT_LOCALE

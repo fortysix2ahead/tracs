8f2cf05a7080dc012cdd9dc5c3ad82628eaf5c31
fortysix2ahead <fortysix2ahead@gmail.com>
2026-07-09 13:26:55 +0200

    Comment @resource_type in MockActivity

--- test/mock.py
+++ test/mock.py
@@ -24,7 +24,7 @@ MINIMAL_GPX = \
 	r'<wpt lat="37.778259000" lon="-122.391386000"><time>2016-06-17T23:41:03Z</time></wpt>' \
 	r'</gpx>'
 
-@resource_type( type=MOCK_TYPE, summary=True )
+# @resource_type( type=MOCK_TYPE, summary=True )
 @define
 class MockActivity:
 
